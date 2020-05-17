10b03a98e0e7220ff43235bc0ef61c61304d902d
shusuke0812 <shu.ota0812@gmail.com>
2020-05-17 23:56:51 +0900

    Home画面のUI部品を配置

--- KikurageProjectForiOS/HomeView.swift
+++ KikurageProjectForiOS/HomeView.swift
@@ -10,7 +10,53 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                Image(systemName: "line.horizontal.3")
+                    .foregroundColor(.blue)
+                Spacer()
+                Text("今日のきくらげ")
+                Spacer()
+            }.padding()
+            Text("Hello, World!")
+            Text("今日のきんちゃん")
+            Text("のどカラカラや！")
+            Image("Normal-0")
+                .padding()
+            HStack {
+                Text("")
+                Text("温度")
+                Text("湿度")
+            }
+            HStack {
+                Text("現在")
+                Text("24℃")
+                Text("45%")
+            }
+            HStack {
+                Text("理想")
+                Text("20-25℃")
+                Text("80%以上")
+            }
+            Spacer()
+            HStack {
+                Button(action: {
+                    
+                }) {
+                    Text("さいばいきろく")
+                }
+                Button(action: {
+                    
+                }) {
+                    Text("りょうりきろく")
+                }
+                Button(action: {
+                    
+                }) {
+                    Text("みんなにそうだん")
+                }
+            }
+        }
     }
 }
 
