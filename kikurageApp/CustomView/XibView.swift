77e509f5e499f9ae4f98e5cd372843d77e489cdd
shusuke0812 <shu.ota0812@gmail.com>
2020-10-09 19:42:20 +0900

    [fix]UIViewのBaseクラスを編集

--- kikurageApp/CustomView/XibView.swift
+++ kikurageApp/CustomView/XibView.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 shusuke. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class XibView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.loadView()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.loadView()
+    }
+    private func loadView() {
+        let className: String = String(describing: type(of: self))
+        let view: UIView = Bundle.main.loadNibNamed(className, owner: self, options: nil)?.first as! UIView
+        view.backgroundColor = .clear
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
