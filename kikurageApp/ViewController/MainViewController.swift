6ec7cfa4c6eaa69173d6c34578a41026e38d4b3a
shusuke0812 <shu.ota0812@gmail.com>
2020-10-15 15:02:03 +0900

    [add]コメント追記

--- kikurageApp/ViewController/MainViewController.swift
+++ kikurageApp/ViewController/MainViewController.swift
@@ -88,7 +88,7 @@ extension MainViewController {
 // きくらげ表情の表示
 extension MainViewController {
     private func displayKikurageStateImage() {
-        // TODO：温度湿度によって表情を変える処理を書く
+        // TODO：温度湿度によって表情を変える処理を書く（dry,normal,wetの状態判定はサーバーで処理）
         var kikurageStateImages: [UIImage] = []
         let beforeNormaImage: UIImage = UIImage(named: "normal_01")!
         let afterNormalImage: UIImage = UIImage(named: "normal_02")!
