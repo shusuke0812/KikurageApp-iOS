d6602a3a777b0c5d0c1936bd334e68990b5764b8
shusuke0812 <shu.ota0812@gmail.com>
2020-10-21 19:46:07 +0900

    [fix]きくらげの状態を取得して保持するViewModelのコードを追記

--- kikurageApp/ViewModel/MainViewModel.swift
+++ kikurageApp/ViewModel/MainViewModel.swift
@@ -8,5 +8,44 @@
 
 import UIKit
 
+protocol MainViewModelDelgate: class {
+    /// きくらげの状態データ取得に成功した
+    func didSuccessGetKikurageState()
+    /// きくらげの状態データ取得に失敗した
+    /// - Parameter errorMessage: エラーメッセージ
+    func didFailedGetKikurageState(errorMessage: String)
+}
+
 class MainViewModel {
+    /// きくらげの状態取得リポジトリ
+    private let kikurageStateRepository: KikurageStateRepositoryProtocol
+    /// きくらげの状態
+    private var kikurageState: KikurageState?
+    /// デリゲート
+    internal weak var delegate: MainViewModelDelgate?
+    
+    init(kikurageStateRepository: KikurageStateRepositoryProtocol) {
+        self.kikurageStateRepository = kikurageStateRepository
+    }
+}
+
+extension MainViewModel {
+    /// きくらげの状態を読み込む
+    func loadKikurageState() {
+        self.kikurageStateRepository.getKikurageState(
+            uid: "BFwAuLtNWTg3YKhOqYsj",
+            completion: { response in
+                switch response {
+                case .success(let kikurageState):
+                    DispatchQueue.main.async {
+                        [weak self] in
+                        self?.kikurageState = kikurageState
+                        self?.delegate?.didSuccessGetKikurageState()
+                    }
+                case .failure(let error):
+                    print("DEBUG: \(error)")
+                    self.delegate?.didFailedGetKikurageState(errorMessage: "きくらげの状態を取得できませんでした")
+                }
+            })
+    }
 }
