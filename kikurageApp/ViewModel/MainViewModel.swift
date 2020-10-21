110e6f64eba27dccb0a5d37705300fe96c963442
shusuke0812 <shu.ota0812@gmail.com>
2020-10-21 19:25:52 +0900

    [add]トップ画面のViewModelファイルを追加

---
+++ kikurageApp/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/10/21.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class MainViewModel {
+}
