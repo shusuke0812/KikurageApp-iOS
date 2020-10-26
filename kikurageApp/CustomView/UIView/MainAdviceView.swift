0c4a3964f831debe27d64350b00e6ec248f06fb1
shusuke0812 <shu.ota0812@gmail.com>
2020-10-26 21:20:17 +0900

    [add]トップ画面の栽培アドバイス表示用のViewファイルを作成

---
+++ kikurageApp/CustomView/UIView/MainAdviceView.swift
@@ -0,0 +1,12 @@
+//
+//  MainAdviceView.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/10/26.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class MainAdviceView: XibView {
+}
