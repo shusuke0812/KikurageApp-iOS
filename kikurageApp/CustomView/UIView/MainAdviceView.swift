65ecc9fad83008b2cefbcfa4b515f1acc557c203
shusuke0812 <shu.ota0812@gmail.com>
2020-10-27 09:40:19 +0900

    [fix]トップ画面の栽培アドバイス表示用のViewファイルを修正

--- kikurageApp/CustomView/UIView/MainAdviceView.swift
+++ kikurageApp/CustomView/UIView/MainAdviceView.swift
@@ -9,4 +9,34 @@
 import UIKit
 
 class MainAdviceView: XibView {
+    @IBOutlet weak var baseView: UIView!
+    @IBOutlet weak var adviceIconImageView: UIImageView!
+    @IBOutlet weak var adviceTitleLabel: UILabel!
+    @IBOutlet weak var adviceContentLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initBaseView()
+        self.initAdviceTitleLabel()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MainAdviceView {
+    private func initBaseView() {
+        // BaseViewを角丸にする
+        self.baseView.layer.cornerRadius = 18
+        // BaseViewの枠線に色をつける
+        self.baseView.layer.borderColor = UIColor.red.cgColor
+        // BaseViewの枠線の太さを設定する
+        self.baseView.layer.borderWidth = 1.0
+    }
+    private func initAdviceTitleLabel() {
+        self.adviceTitleLabel.text = "【アドバイス】"
+    }
+    private func initAdviceIconImageView() {
+        self.adviceIconImageView.image = UIImage(named: "hakase")
+    }
 }
