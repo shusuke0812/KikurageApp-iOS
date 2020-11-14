70492371a97775dde67d8f9686dc6f4a31144b98
shusuke0812 <shu.ota0812@gmail.com>
2020-11-14 15:39:44 +0900

    [fix]きくらげ栽培記録データ格納用の構造体を設定

--- kikurageApp/Entity/KikurageCultivation.swift
+++ kikurageApp/Entity/KikurageCultivation.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2020 shusuke. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+import SwiftDate
+
+struct KikurageCultivation {
+    /// 栽培メモ
+    var memo: String = ""
+    /// 栽培写真
+    var imageStoragePath: [String] = []
+    /// 栽培観察日
+    var viewDate: String = ""
+    /// 投稿日
+    var createdAt: Timestamp?
+    /// 更新日
+    var updatedAt: Timestamp?
+    
+    enum CodingKeys: String, CodingKey {
+        case memo
+        case imageStoragePath
+        case viewDate
+        case createdAt
+        case updatedAt
+    }
+}
