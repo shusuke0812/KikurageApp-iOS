ccfddf386b6c462231f3c802b605844807324c5c
shusuke0812 <shu.ota0812@gmail.com>
2020-11-14 16:19:35 +0900

    [fix]きくらげ栽培データ保存用のリポジトリを修正

--- kikurageApp/Repository/CultivationRepository.swift
+++ kikurageApp/Repository/CultivationRepository.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2020 shusuke. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+protocol CultivationRepositoryProtocol {
+    /// 栽培記録を投稿する
+    /// - Parameters:
+    ///   - kikurageUserId: ユーザーID
+    ///   - cultivation: Firestoreへ保存する栽培記録データ
+    ///   - completion: 投稿成功、失敗のハンドル
+    func postCultivation(kikurageUserId: String,
+                         kikurageCultivation: KikurageCultivation,
+                         completion: @escaping (Result<DocumentReference, Error>) -> Void)
+}
+class CultivationRepository: CultivationRepositoryProtocol {
+}
+extension CultivationRepository {
+    func postCultivation(kikurageUserId: String,
+                         kikurageCultivation: KikurageCultivation,
+                         completion: @escaping (Result<DocumentReference, Error>) -> Void) {
+        let db = Firestore.firestore()
+        var data: [String: Any]!
+        do {
+            data = try Firestore.Encoder().encode(kikurageCultivation)
+        } catch (let error) {
+            fatalError(error.localizedDescription)
+        }
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+        let documentReference: DocumentReference = db.collection(Constants.FirestoreCollectionName.users).document(kikurageUserId).collection(Constants.FirestoreCollectionName.cultivations).addDocument(data: data) { error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            dispatchGroup.leave()
+        }
+        // Firestoreにデータを登録した後にFirestore登録用にドキュメントIDをコールバックする
+        dispatchGroup.notify(queue: .main) {
+            completion(.success(documentReference))
+        }
+    }
+}
