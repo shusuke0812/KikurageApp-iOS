da39b9a18fbec03f1ea1a04b1e3ad20dc90d3223
shusuke0812 <shu.ota0812@gmail.com>
2020-11-18 00:56:16 +0900

    [add]栽培記録投稿画面に日付選択フィールドと保存ボタンを追加

--- kikurageApp/CustomView/BaseView/PostCultivationBaseView.swift
+++ kikurageApp/CustomView/BaseView/PostCultivationBaseView.swift
@@ -8,22 +8,39 @@
 
 import UIKit
 
+protocol PostCultivationBaseViewDelegate: class {
+    /// 栽培記録保存ボタンが押された時の処理
+    func didTapPostButton()
+}
+
 class PostCultivationBaseView: UIView {
     
     @IBOutlet weak var cameraCollectionView: UICollectionView!
     @IBOutlet weak var textView: UITextViewWithPlaceholder!
     @IBOutlet weak var currentTextViewNumberLabel: UILabel!
     @IBOutlet weak var maxTextViewNumberLabel: UILabel!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var postButton: UIButton!
     
+    /// デリゲート
+    internal weak var delegate: PostCultivationBaseViewDelegate?
+    
+    // MARK: - Lifecycle
     override func awakeFromNib() {
         super.awakeFromNib()
         self.initUI()
+    }
+    // MARK: - Action Method
+    @IBAction func didTapPostButton(_ sender: Any) {
+        self.delegate?.didTapPostButton()
     }
 }
 // MARK: - Initialized Method
 extension PostCultivationBaseView {
     private func initUI() {
         self.textView.placeholder = "観察メモ"
+        self.postButton.layer.masksToBounds = true
+        self.postButton.layer.cornerRadius = 5
     }
 }
 
