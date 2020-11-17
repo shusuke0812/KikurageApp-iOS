0c263427159d3a303dc027567e78a42947d5a8fa
shusuke0812 <shu.ota0812@gmail.com>
2020-11-17 23:21:37 +0900

    [add]栽培記録投稿用のViewModelを追加

--- kikurageApp/ViewModel/PostCultivationViewModel.swift
+++ kikurageApp/ViewModel/PostCultivationViewModel.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2020 shusuke. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol PostCultivationViewModelDelegate: class {
+    /// 栽培記録の投稿に成功した
+    func didSuccessPostCultivation()
+    /// 栽培記録の投稿に失敗した
+    func didFailedPostCultivation(errorMessage: String)
+}
+class PostCultivationViewModel {
+    /// 栽培記録リポジトリ
+    private let cultivationRepository: CultivationRepositoryProtocol
+    /// デリゲート
+    internal weak var delegate: PostCultivationViewModelDelegate?
+    
+    init(cultivationRepository: CultivationRepositoryProtocol) {
+        self.cultivationRepository = cultivationRepository
+    }
+}
+extension PostCultivationViewModel {
+    func postCultivation(kikurageUserId: String,
+                         kikurageCultivation: KikurageCultivation) {
+        self.cultivationRepository
+            .postCultivation(kikurageUserId: kikurageUserId, kikurageCultivation: kikurageCultivation, completion: { response in
+                switch response {
+                case .success(let documentReference):
+                    print(documentReference)
+                    self.delegate?.didSuccessPostCultivation()
+                case .failure(let error):
+                    print("DEBUG: \(error)")
+                }
+            })
+    }
+}
