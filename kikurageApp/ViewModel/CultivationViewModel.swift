460cba7cf76fcfeebc55dac28219a991728d04c4
shusuke0812 <shu.ota0812@gmail.com>
2020-11-17 23:19:24 +0900

    [add]栽培記録投稿用のViewModelを追加

--- kikurageApp/ViewModel/CultivationViewModel.swift
+++ kikurageApp/ViewModel/CultivationViewModel.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 shusuke. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+protocol CultivationViewModelDelegate: class {
+    /// きくらげ栽培記録の取得に成功した
+    func didSuccessGetKikurageUserCultivations()
+    /// きくらげ栽培記録の取得に失敗した
+    func didFailedGetKikurageUserCultivations()
+}
+class CultivationViewModel {
+    /// きくらげ栽培記録の取得リポジトリ
+    //private let kikurageUserCultivationRepository: KikurageUserCultivationRepositoryProtocol
+    /// デリゲート
+    internal weak var delegate: CultivationViewModelDelegate?
+    /*
+    init(kikurageUserCultivationRepository: KikurageUserCultivationRepositoryProtocol) {
+        self.kikurageUserCultivationRepository = kikurageUserCultivationRepository
+    }
+    */
+}
+extension CultivationViewModel {
+    /// きくらげ栽培記録を読み込む
+    func loadKikurageUserCultivations(kikurageUserId: String) {
+    }
+}
