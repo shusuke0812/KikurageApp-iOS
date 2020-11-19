0d4435d91c408b64d1bf77984cda696547f2a221
shusuke0812 <shu.ota0812@gmail.com>
2020-11-19 11:48:07 +0900

    [add]Date型をString型に変更するHelperクラスを追加

---
+++ kikurageApp/Helper/DateHelper.swift
@@ -0,0 +1,18 @@
+//
+//  DateHelper.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/11/19.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import Foundation
+
+class DateHelper {
+    /// Dateを指定したStringに変換する
+    internal func formatToString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return "\(formatter.string(from: date))"
+    }
+}
