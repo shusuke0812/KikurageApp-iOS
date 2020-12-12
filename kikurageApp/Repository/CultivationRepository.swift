cb6eb3133eb2a0fdb4d3ea7ae902d881d1ab8f71
shusuke0812 <shu.ota0812@gmail.com>
2020-12-12 11:59:25 +0900

    [fix]栽培画像投稿メソッドの同期・非同期処理を修正

--- kikurageApp/Repository/CultivationRepository.swift
+++ kikurageApp/Repository/CultivationRepository.swift
@@ -22,8 +22,9 @@ protocol CultivationRepositoryProtocol {
     /// - Parameters:
     ///   - imageData: 保存する画像データ
     ///   - imageStoragePath: 画像を保存するStorageパス
+    ///   - completion: 投稿成功、失敗のハンドル
     func postCultivationImages(imageData: [Data?], imageStoragePath: String,
-                               completion: @escaping (Result<String, Error>) -> Void)
+                               completion: @escaping (Result<[String], Error>) -> Void)
 }
 class CultivationRepository: CultivationRepositoryProtocol {
     // DateHelper
@@ -66,26 +67,34 @@ extension CultivationRepository {
 // MARK: - Firebase Storage Method
 extension CultivationRepository {
     func postCultivationImages(imageData: [Data?], imageStoragePath: String,
-                               completion: @escaping (Result<String, Error>) -> Void) {
-        let dispathcGroup = DispatchGroup()
-        let dispatchQueue = DispatchQueue(label: "PostImageTask")
-        for (i, imageData) in zip(imageData.indices, imageData) {
-            guard let imageData = imageData else { return }
-            dispathcGroup.enter()
-            dispatchQueue.async(group: dispathcGroup) {
+                               completion: @escaping (Result<[String], Error>) -> Void) {
+        // 画像保存後のフルパス格納用
+        var imageStorageFullPath: [String] = Array(repeating: "", count: imageData.count)
+        // 直列処理（画像を１つずつ保存する）
+        let dispatchSemaphore = DispatchSemaphore(value: 0)
+        let dispatchQueue = DispatchQueue(label: "com.shusuke.KikurageApp.upload_cultivation_images_queue")
+        dispatchQueue.async {
+            for (i, imageData) in zip(imageData.indices, imageData) {
+                guard let imageData = imageData else {
+                    dispatchSemaphore.signal()
+                    return
+                }
                 let fileName: String = self.dateHelper.formatToStringForImageData(date: Date()) + "_\(i).jpeg"
                 let storageReference = Storage.storage().reference().child(imageStoragePath + fileName)
                 _ = storageReference.putData(imageData, metadata: self.metaData) { (metaData, error) in
                     if let error = error {
                         completion(.failure(error))
+                        dispatchSemaphore.signal()
                         return
                     }
-                    dispathcGroup.leave()
+                    imageStorageFullPath.append(storageReference.fullPath)
+                    dispatchSemaphore.signal()
                 }
+                dispatchSemaphore.wait()
             }
-        }
-        dispathcGroup.notify(queue: .main) {
-            completion(.success("DEBUG: 画像の保存に成功しました"))
+            DispatchQueue.main.async {
+                completion(.success(imageStorageFullPath))
+            }
         }
     }
 }
