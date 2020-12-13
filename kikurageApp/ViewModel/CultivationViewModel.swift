6b10184e374a27f6705ca16592e90bc56930a897
shusuke0812 <shu.ota0812@gmail.com>
2020-12-14 00:23:31 +0900

    [add/fix]栽培記録データ保持用のViewModelを修正
    ・デリゲートメソッド名を変更
    ・栽培記録取得リポジトリと接続
    ・栽培記録取得メソッドとデータを保持するプロパティ変数を追加

--- kikurageApp/ViewModel/CultivationViewModel.swift
+++ kikurageApp/ViewModel/CultivationViewModel.swift
@@ -11,23 +11,36 @@ import Firebase
 
 protocol CultivationViewModelDelegate: class {
     /// きくらげ栽培記録の取得に成功した
-    func didSuccessGetKikurageUserCultivations()
+    func didSuccessGetCultivations()
     /// きくらげ栽培記録の取得に失敗した
-    func didFailedGetKikurageUserCultivations()
+    /// - Parameter errorMessage: エラーメッセージ
+    func didFailedGetCultivations(errorMessage: String)
 }
 class CultivationViewModel {
     /// きくらげ栽培記録の取得リポジトリ
-    //private let kikurageUserCultivationRepository: KikurageUserCultivationRepositoryProtocol
+    private let cultivationRepository: CultivationRepositoryProtocol
     /// デリゲート
     internal weak var delegate: CultivationViewModelDelegate?
-    /*
-    init(kikurageUserCultivationRepository: KikurageUserCultivationRepositoryProtocol) {
-        self.kikurageUserCultivationRepository = kikurageUserCultivationRepository
+    /// きくらげ栽培記録データ
+    var cultivations: [(cultivation: KikurageCultivation, documentId: String)] = []
+    
+    init(cultivationRepository: CultivationRepositoryProtocol) {
+        self.cultivationRepository = cultivationRepository
     }
-    */
 }
+// MARK: - Firebase Firestore Method
 extension CultivationViewModel {
     /// きくらげ栽培記録を読み込む
-    func loadKikurageUserCultivations(kikurageUserId: String) {
+    func loadCultivations(kikurageUserId: String) {
+        self.cultivationRepository
+            .getCultivations(kikurageUserId: kikurageUserId,
+                             completion: { [weak self] response in
+                                switch response {
+                                case .success(let cultivations):
+                                    self?.cultivations = cultivations
+                                case .failure(let error):
+                                    self?.delegate?.didFailedGetCultivations(errorMessage: "\(error)")
+                                }
+                             })
     }
 }
