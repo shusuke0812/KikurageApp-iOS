22876fd424412a34b3610a6e64f9e05eb9de04e8
shusuke0812 <shu.ota0812@gmail.com>
2020-12-19 08:23:34 +0900

    [add]栽培記録詳細画面のViewController, View, Storyboardファイルを追加

---
+++ kikurageApp/CustomView/BaseView/CultivationDetailBaseView.swift
@@ -0,0 +1,12 @@
+//
+//  CultivationDetailBaseView.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/12/19.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class CultivationDetailBaseView: UIView {
+}
---
+++ kikurageApp/ViewController/Cultivation/CultivationDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CultivationDetailViewController.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/12/19.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class CultivationDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
