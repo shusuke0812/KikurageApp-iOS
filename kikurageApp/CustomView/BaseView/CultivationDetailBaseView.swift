e6d757d54f3f1b24445935e1d3c46e2fddccf426
shusuke0812 <shu.ota0812@gmail.com>
2020-12-21 18:07:45 +0900

    [add]栽培記録詳細画面にUIパーツを設置

--- kikurageApp/CustomView/BaseView/CultivationDetailBaseView.swift
+++ kikurageApp/CustomView/BaseView/CultivationDetailBaseView.swift
@@ -9,4 +9,22 @@
 import UIKit
 
 class CultivationDetailBaseView: UIView {
+    
+    @IBOutlet weak var viewDateLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var memoTitleLabel: UILabel!
+    @IBOutlet weak var memoLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.initUI()
+    }
+}
+// MARK: - Initialized Method
+extension CultivationDetailBaseView {
+    private func initUI() {
+        self.viewDateLabel.text = ""
+        self.memoTitleLabel.text = "観察メモ"
+        self.memoLabel.text = ""
+    }
 }
