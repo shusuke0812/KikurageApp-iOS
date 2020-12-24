d6ef9efe9ae6f15cb9c7b49067d50a975d33a2d1
shusuke0812 <shu.ota0812@gmail.com>
2020-12-24 13:17:42 +0900

    [add]栽培記録詳細画面のViewModelを追加

---
+++ kikurageApp/ViewModel/CultivationDetailViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CultivationDetailViewModel.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/12/24.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class CultivationDetailViewModel: NSObject {
+    /// きくらげ 栽培記録データ
+    var cultivation: KikurageCultivation!
+}
+// MARK: - UICollectionView DataSource Method
+extension CultivationDetailViewModel: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.cultivation.imageStoragePaths.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CultivationCarouselCollectionView", for: indexPath) as! CultivationCarouselCollectionViewCell
+        cell.setUI(cultivationImageStoragePath: self.cultivation.imageStoragePaths[indexPath.row])
+        return cell
+    }
+}
