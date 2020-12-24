f91aa230b68d939fb0d81391aae9933368f29cae
shusuke0812 <shu.ota0812@gmail.com>
2020-12-24 13:28:52 +0900

    [add]栽培記録詳細画面にCollectionView系の処理を追加
    ・ViewModelと接続
    ・FlowLayoutのセルサイズ指定メソッドを追加
    ・CollectionViewのDelegateメソッド追加

--- kikurageApp/ViewController/Cultivation/CultivationDetailViewController.swift
+++ kikurageApp/ViewController/Cultivation/CultivationDetailViewController.swift
@@ -12,9 +12,13 @@ class CultivationDetailViewController: UIViewController {
     
     /// BaseView
     private var baseView: CultivationDetailBaseView { self.view as! CultivationDetailBaseView }
+    /// ViewModel
+    private var viewModel: CultivationDetailViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = CultivationDetailViewModel()
+        self.setDelegateDataSource()
         self.setNavigationItem()
     }
 }
@@ -22,5 +26,24 @@ class CultivationDetailViewController: UIViewController {
 extension CultivationDetailViewController {
     private func setNavigationItem() {
         self.setNavigationBar(title: "さいばいきろく詳細")
+    }
+    private func setDelegateDataSource() {
+        self.baseView.collectionView.delegate = self
+        self.baseView.collectionView.dataSource = self.viewModel
+    }
+}
+// MARK: - UICollectionView Delegate Method
+extension CultivationDetailViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // TODO: 画像拡大処理を書く
+    }
+}
+// MARK: - UICollectionView Delegate FlowLayout Method
+extension CultivationDetailViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let horiizontalSpace: CGFloat = 0.0
+        let cellWidth: CGFloat = self.baseView.bounds.width - horiizontalSpace
+        let cellHeight: CGFloat = cellWidth
+        return CGSize(width: cellWidth, height: cellHeight)
     }
 }
