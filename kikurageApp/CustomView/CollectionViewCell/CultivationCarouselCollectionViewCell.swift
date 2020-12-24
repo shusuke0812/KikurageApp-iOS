65c70527853da6ffcb06da5cca4e547572d87e57
shusuke0812 <shu.ota0812@gmail.com>
2020-12-24 10:54:08 +0900

    [add]栽培記録詳細画面のカルーセル画像表示のファイルを追加

---
+++ kikurageApp/CustomView/CollectionViewCell/CultivationCarouselCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  CultivationCarouselCollectionViewCell.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/12/24.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+
+class CultivationCarouselCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.initUI()
+    }
+}
+// MARK: - Initialized Method
+extension CultivationCarouselCollectionViewCell {
+    private func initUI() {
+        self.imageView.backgroundColor = .lightGray
+    }
+}
+// MARK: - Setting UI Method
+extension CultivationCarouselCollectionViewCell {
+    func setUI(cultivationImageStoragePath: String) {
+        let storageReference = Storage.storage().reference(withPath: cultivationImageStoragePath)
+        self.imageView.sd_setImage(with: storageReference)
+    }
+}
