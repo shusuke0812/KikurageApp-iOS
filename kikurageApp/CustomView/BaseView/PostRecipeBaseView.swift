890d61ef810b17ba93ec5daaa986bda4971a0ffa
shusuke0812 <shu.ota0812@gmail.com>
2020-12-28 18:44:31 +0900

    [add]料理記録投稿画面のViewにUIパーツを接続

--- kikurageApp/CustomView/BaseView/PostRecipeBaseView.swift
+++ kikurageApp/CustomView/BaseView/PostRecipeBaseView.swift
@@ -8,14 +8,43 @@
 
 import UIKit
 
+protocol PostRecipeBaseViewDelegate: class {
+    /// 料理記録を保存するボタンを押した時の処理
+    func didTapPostButton()
+    /// 閉じるボタンを押した時の処理
+    func didTapCloseButton()
+}
+
 class PostRecipeBaseView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var navigationItem: UINavigationBar!
+    @IBOutlet weak var cameraCollectionView: UICollectionView!
+    @IBOutlet weak var recipeNameTextField: UITextField!
+    @IBOutlet weak var currentRecipeNameNumberLabel: UILabel!
+    @IBOutlet weak var maxRecipeNameNumberLabel: UILabel!
+    @IBOutlet weak var recipeMemoTextView: UITextViewWithPlaceholder!
+    @IBOutlet weak var currentRecipeMemoNumberLabel: UILabel!
+    @IBOutlet weak var maxRecipeMemoNumberLabel: UILabel!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var postButton: UIButton!
+    /// デリゲート
+    internal weak var delegate: PostRecipeBaseViewDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
     }
-    */
-
+    // MARK: - Action Method
+    @IBAction func didTapCloseButton(_ sender: Any) {
+        self.delegate?.didTapCloseButton()
+    }
+    @IBAction func didTapPostButton(_ sender: Any) {
+        self.delegate?.didTapPostButton()
+    }
+}
+// MARK: - Initialized Method
+extension PostRecipeBaseView {
+    
+}
+// MARK: - Setting UI Method
+extension PostRecipeBaseView {
+    
 }
