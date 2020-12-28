4592be262bec55501908b5f4d051a697342cee14
shusuke0812 <shu.ota0812@gmail.com>
2020-12-28 18:43:40 +0900

    [add]料理記録投稿画面に必要なStoryboard, ViewController, Viewファイルを追加

---
+++ kikurageApp/CustomView/BaseView/PostRecipeBaseView.swift
@@ -0,0 +1,21 @@
+//
+//  PostRecipeBaseView.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/12/28.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class PostRecipeBaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ kikurageApp/ViewController/Recipe/PostRecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostRecipeViewController.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2020/12/28.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class PostRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
