371230eb449840dc9b5549028d3d8b91bcb0874e
shusuke0812 <shu.ota0812@gmail.com>
2021-01-01 11:01:29 +0900

    [add]料理記録投稿画面のViewControllerにViewとViewModelを接続し、delegateメソッドを追加

--- kikurageApp/ViewController/Recipe/PostRecipeViewController.swift
+++ kikurageApp/ViewController/Recipe/PostRecipeViewController.swift
@@ -9,22 +9,73 @@
 import UIKit
 
 class PostRecipeViewController: UIViewController {
+    
+    /// BaseView
+    private var baseView: PostRecipeBaseView { self.view as! PostRecipeBaseView }
+    /// ViewModel
+    private var viewModel: PostRecipeViewModel!
+    /// CameraCell ViewModel
+    private var cameraCollectionViewModel: CameraCollectionViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.cameraCollectionViewModel = CameraCollectionViewModel(selectedImageMaxNumber: Constants.CameraCollectionCell.maxNumber)
+        self.viewModel = PostRecipeViewModel(recipeRepository: RecipeRepository())
+        self.setDelegateDataSource()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+// MARK: - Initialized Method
+extension PostRecipeViewController {
+    private func setDelegateDataSource() {
+        self.baseView.delegate = self
+        self.baseView.recipeNameTextField.delegate = self
+        self.baseView.dateTextField.delegate = self
+        self.baseView.cameraCollectionView.delegate = self
+        self.baseView.cameraCollectionView.dataSource = self.cameraCollectionViewModel
+        self.cameraCollectionViewModel.cameraCellDelegate = self
+        self.viewModel.delegate = self
     }
-    */
-
+}
+// MARK: - BaseView Delegate Method
+extension PostRecipeViewController: PostRecipeBaseViewDelegate {
+    func didTapPostButton() {
+        
+    }
+    func didTapCloseButton() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+// MARK: - UITextField Delegate Method
+extension PostRecipeViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField.tag {
+        case Constants.TextFieldTag.recipeDate:
+            print("")
+        case Constants.TextFieldTag.recipeName:
+            print("")
+        default:
+            break
+        }
+    }
+}
+// MARK: - CameraCell Delegate Method
+extension PostRecipeViewController: CameraCellDelegate {
+    func didTapImageCancelButton(cell: CameraCell) {
+    }
+}
+// MARK: - UICollectionView Delegate Method
+extension PostRecipeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+}
+// MARK: - PostRecipeViewModel Method
+extension PostRecipeViewController: PostRecipeViewModelDelegate {
+    func didSuccessPostRecipe() {
+    }
+    func didFailedPostRecipe(errorMessage: String) {
+    }
+    func didSuccessPostRecipeImages() {
+    }
+    func didFailedPostRecipeImages(errorMessage: String) {
+    }
 }
