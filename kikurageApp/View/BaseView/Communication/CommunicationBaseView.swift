6c58ab69ec2db825269e3c0f29dbcd3c84872fc4
shusuke0812 <shu.ota0812@gmail.com>
2021-01-07 09:17:20 +0900

    [add]相談画面のViewとカラーassetを追加

---
+++ kikurageApp/View/BaseView/Communication/CommunicationBaseView.swift
@@ -0,0 +1,21 @@
+//
+//  CommunicationBaseView.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2021/1/7.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class CommunicationBaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
