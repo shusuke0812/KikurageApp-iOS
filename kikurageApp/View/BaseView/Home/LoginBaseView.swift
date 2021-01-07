59923206ae78f53ab5c84bff60df397ac38e984c
shusuke0812 <shu.ota0812@gmail.com>
2021-01-07 09:42:15 +0900

    [add]ログイン画面のViewファイルを追加

---
+++ kikurageApp/View/BaseView/Home/LoginBaseView.swift
@@ -0,0 +1,12 @@
+//
+//  LoginBaseView.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2021/1/7.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class LoginBaseView: UIView {
+}
