f1807e3fe8654c98e85dc530ccf8dcfe955d3068
shusuke0812 <shu.ota0812@gmail.com>
2021-01-08 10:07:32 +0900

    [fix]トップ画面のViewModelからユーザー取得処理を削除（ユーザーデータは前画面より受け取るようにする）

--- kikurageApp/ViewModel/Home/MainViewModel.swift
+++ kikurageApp/ViewModel/Home/MainViewModel.swift
@@ -17,11 +17,6 @@ protocol MainViewModelDelgate: class {
     func didFailedGetKikurageState(errorMessage: String)
     /// きくらげの状態データが更新された
     func didChangedKikurageState()
-    /// きくらげユーザーの取得に成功した
-    func didSuccessGetKikurageUser()
-    /// きくらげユーザーの取得に失敗した
-    /// - Parameter errorMessage: エラーメッセージ
-    func didFailedGetKikurageUser(errorMessage: String)
 }
 
 class MainViewModel {
@@ -29,35 +24,29 @@ class MainViewModel {
     private var kikurageStateListener: ListenerRegistration?
     /// きくらげの状態取得リポジトリ
     private let kikurageStateRepository: KikurageStateRepositoryProtocol
-    /// きくらげユーザー取得リポジトリ
-    private let kikurageUserRepository: KikurageUserRepositoryProtocol
     /// きくらげの状態
-    internal var kikurageState: KikurageState?
+    var kikurageState: KikurageState?
+    /// きくらげの状態ID（プロダクトキー ）
+    var kikuragestateId: String!
     /// きくらげユーザー
-    internal var kikurageUser: KikurageUser?
+    var kikurageUser: KikurageUser?
     /// デリゲート
     internal weak var delegate: MainViewModelDelgate?
     
-    // テスト用ID（後で消す）
-    let productId: String = "BFwAuLtNWTg3YKhOqYsj"
-    let userId: String = "i0GrcLgkBBoLrBgGtrjp"
-    
-    init(kikurageStateRepository: KikurageStateRepositoryProtocol,
-         kikurageUserRepository: KikurageUserRepositoryProtocol) {
+    init(kikurageStateRepository: KikurageStateRepositoryProtocol) {
         self.kikurageStateRepository = kikurageStateRepository
-        self.kikurageUserRepository = kikurageUserRepository
         self.setKikurageStateListener()
     }
     deinit {
         self.kikurageStateListener?.remove()
     }
 }
-// MARK: - Setting Kikurage State Data Method
+// MARK: - Firebase Firestore Method
 extension MainViewModel {
     /// きくらげの状態を読み込む
     func loadKikurageState() {
         self.kikurageStateRepository
-            .getKikurageState(productId: self.productId,
+            .getKikurageState(productId: self.kikuragestateId,
                               completion: { response in
                                 switch response {
                                 case .success(let kikurageState):
@@ -73,7 +62,7 @@ extension MainViewModel {
     }
     /// きくらげの状態を監視して更新を通知する
     func setKikurageStateListener() {
-        self.kikurageStateListener = Firestore.firestore().collection("kikurageStates").document(self.productId).addSnapshotListener { [weak self] (snapshot, error) in
+        self.kikurageStateListener = Firestore.firestore().collection("kikurageStates").document(self.kikuragestateId).addSnapshotListener { [weak self] (snapshot, error) in
             guard let snapshot: DocumentSnapshot = snapshot else {
                 print("DEBUG: \(error!)")
                 return
@@ -90,24 +79,5 @@ extension MainViewModel {
                 fatalError(error.localizedDescription)
             }
         }
-    }
-}
-// MARK: - Setting Kikurage User Data Method
-extension MainViewModel {
-    func loadKikurageUser() {
-        self.kikurageUserRepository
-            .getKikurageUser(uid: userId,
-                             completion: { responsse in
-                                 switch responsse {
-                                 case .success(let kikurageUser):
-                                     DispatchQueue.main.async { [weak self] in
-                                        self?.kikurageUser = kikurageUser
-                                        self?.delegate?.didSuccessGetKikurageUser()
-                                     }
-                                 case .failure(let error):
-                                     print("DEBUG: \(error)")
-                                     self.delegate?.didFailedGetKikurageUser(errorMessage: "きくらげユーザーを取得できませんでした")
-                                 }
-                             })
     }
 }
