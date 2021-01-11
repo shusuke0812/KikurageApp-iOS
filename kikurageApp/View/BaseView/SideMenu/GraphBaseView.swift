b98cc6bd38375c4f09925445ce0799735f2a0beb
shusuke0812 <shu.ota0812@gmail.com>
2021-01-11 14:09:23 +0900

    [add]グラフ画面のViewにて、タイトルと閉じるボタンの処理を追加

--- kikurageApp/View/BaseView/SideMenu/GraphBaseView.swift
+++ kikurageApp/View/BaseView/SideMenu/GraphBaseView.swift
@@ -8,9 +8,29 @@
 
 import UIKit
 
+protocol GraphBaaseViewDelegate: class {
+    /// 閉じるボタンを押した時の処理
+    func didTapCloseButton()
+}
+
 class GraphBaseView: UIView {
+    
+    @IBOutlet weak var navigationItem: UINavigationItem!
+    /// デリゲート
+    internal weak var delegate: GraphBaaseViewDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    // MARK: - Action Method
+    @IBAction func didTapCloseButton(_ sender: Any) {
+        self.delegate?.didTapCloseButton()
+    }
+}
+// MARK: - Initialized Method
+extension GraphBaseView {
+    private func initUI() {
+        // タイトル
+        self.navigationItem.title = "グラフ"
     }
 }
