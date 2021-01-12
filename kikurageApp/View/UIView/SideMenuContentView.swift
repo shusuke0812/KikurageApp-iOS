f474efc6953a834963451122a7a5b34910b31d97
shusuke0812 <shu.ota0812@gmail.com>
2021-01-12 14:43:25 +0900

    [fix]サイドメニューコンテンツViewのUI設定メソッドを修正
    ・枠線を引くメソッドを初期化するのではなく、インスタンス先から設定するようにした
    ・アイコンとタイトルを設定するメソッドを追加した

--- kikurageApp/View/UIView/SideMenuContentView.swift
+++ kikurageApp/View/UIView/SideMenuContentView.swift
@@ -13,21 +13,26 @@ class SideMenuContentView: XibView {
     @IBOutlet weak var sideMenuContentIconView: UIImageView!
     @IBOutlet weak var sideMenuContentLabel: UILabel!
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.setBoarder
-    }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.setBoarder
     }
 }
-
-// MARK: - Initialized Method
+// MARK: - Setting UI Method
 extension SideMenuContentView {
-    private func setBoarder() {
-        // Viewの上下に枠線を引く
-        self.setFrameBoarder(width: 0.5, color: .lightGray, position: .top)
-        self.setFrameBoarder(width: 0.5, color: .lightGray, position: .bottom)
+    /// Viewの上下に枠線を引く
+    /// - Parameters:
+    ///   - topWidth: 上枠線の幅
+    ///   - bottomWidth: 下枠線の幅
+    func setBoarder(topWidth: CGFloat?, bottomWidth: CGFloat?) {
+        if let topWidth = topWidth {
+            self.setFrameBoarder(width: topWidth, color: .lightGray, position: .top)
+        }
+        if let bottomWidth = bottomWidth {
+            self.setFrameBoarder(width: bottomWidth, color: .lightGray, position: .bottom)
+        }
+    }
+    func setSideMenuContent(title: String, imageSystemName: String) {
+        self.sideMenuContentLabel.text = title
+        self.sideMenuContentIconView.image = UIImage(systemName: imageSystemName)
     }
 }
