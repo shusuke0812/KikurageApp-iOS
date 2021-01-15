a9b0a1e8de91d4455fd139f19f2ccada2c136055
shusuke0812 <shu.ota0812@gmail.com>
2021-01-15 09:31:09 +0900

    [add]グラフViewModelにグラフデータを取得するメソッドを追加

--- kikurageApp/ViewModel/SideMenu/GraphViewModel.swift
+++ kikurageApp/ViewModel/SideMenu/GraphViewModel.swift
@@ -8,6 +8,40 @@
 
 import Foundation
 
-class GraphViewModel {
+protocol GraphViewModelDelegate: class {
+    /// きくらげの状態グラフデータの取得に成功した
+    func didSuccessGetKikurageStateGraph()
+    /// きくらげの状態グラフデータの取得に失敗した
+    /// - Parameter errorMessage: エラーメッセージ
+    func didFailedGetKikurageStateGraph(errorMessage: String)
+}
 
+class GraphViewModel {
+    /// きくらげの状態取得リポジトリ
+    private let kikurageStateRepository: KikurageStateRepositoryProtocol
+    /// デリゲート
+    internal weak var delegate: GraphViewModelDelegate?
+    /// きくらげの１週間データ
+    var kikurageStateGraph: [(graph: KikurageStateGraph, documentId: String)] = []
+    
+    init(kikurageStateRepository: KikurageStateRepositoryProtocol) {
+        self.kikurageStateRepository = kikurageStateRepository
+    }
+}
+// MARK: - Firebase Firestore Method
+extension GraphViewModel {
+    /// きくらげステートのグラフデータを読み込む
+    func loadKikurageStateGraph(productId: String) {
+        self.kikurageStateRepository
+            .getKikurageStateGraph(productId: productId,
+                                   completion: { [weak self] response in
+                                    switch response {
+                                    case .success(let graphs):
+                                        self?.kikurageStateGraph = graphs
+                                        self?.delegate?.didSuccessGetKikurageStateGraph()
+                                    case .failure(let error):
+                                        self?.delegate?.didFailedGetKikurageStateGraph(errorMessage: "\(error)")
+                                    }
+                                   })
+    }
 }
