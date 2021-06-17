592b0ec6a4ecb95b000a2f88c94857b30d32166c
shusuke0812 <shu.ota0812@gmail.com>
2021-06-17 15:57:28 +0900

    [add]CrashlyticsのログにユーザーIDを設定するようにした

--- kikurageApp/AppDelegate.swift
+++ kikurageApp/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Firebase初期化
         FirebaseApp.configure()
         print("DEBUG: \(FirebaseApp.app()?.name ?? "App name is nil")")
+        self.configCrashlyticsUserId()
         // IQKeyboard初期化
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.keyboardDistanceFromTextField = 40
@@ -56,5 +57,8 @@ extension AppDelegate {
         guard let vc = R.storyboard.loginViewController.instantiateInitialViewController() else { return }
         self.window?.rootViewController = vc
         self.window?.makeKeyAndVisible()
+    }
+    private func configCrashlyticsUserId() {
+        Crashlytics.crashlytics().setUserID("12345")
     }
 }
