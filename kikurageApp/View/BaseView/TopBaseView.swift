335a16b798dbbd457858a1c4d251e81f74b88ac3
shusuke0812 <shu.ota0812@gmail.com>
2021-09-03 23:47:32 +0900

    add: トップ画面用のVC, Storyoboard, BaseViewを新しく作成

---
+++ kikurageApp/View/BaseView/TopBaseView.swift
@@ -0,0 +1,15 @@
+//
+//  TopBaseView.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2021/9/3.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class TopBaseView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ kikurageApp/ViewController/TopViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TopViewController.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2021/9/3.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+    /// BaseView
+    private var baseView: TopBaseView { self.view as! TopBaseView }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
