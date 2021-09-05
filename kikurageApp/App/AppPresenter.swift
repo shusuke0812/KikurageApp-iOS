27a7c71fdcb71e9d724364b17085de1df5f8b0ea
shusuke0812 <shu.ota0812@gmail.com>
2021-09-05 20:04:20 +0900

    fix: AppPresenterを使い、UserIDがUserDefaultsに登録されているかどうかで画面遷移するようにした

--- kikurageApp/App/AppPresenter.swift
+++ kikurageApp/App/AppPresenter.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+protocol AppPresenterDelegate: AnyObject {
+    /// きくらげユーザーの取得に成功した
+    func didSuccessGetKikurageUser(kikurageUser: KikurageUser)
+    /// きくらげユーザーの取得に失敗した
+    /// - Parameter errorMessage: エラーメッセージ
+    func didFailedGetKikurageUser(errorMessage: String)
+}
+
+class AppPresenter {
+    /// きくらげユーザー取得リポジトリ
+    private let kikurageUserRepository: KikurageUserRepositoryProtocol
+
+    weak var delegate: AppPresenterDelegate?
+
+    init(kikurageUserRepository: KikurageUserRepositoryProtocol) {
+        self.kikurageUserRepository = kikurageUserRepository
+    }
+}
+
+// MARK: - Initialized
+extension AppPresenter {
+    /// ユーザーIDが`UserDefaults`に登録されているか確認し、ある場合はユーザーを取得する
+    func checkSavedUserId() {
+        guard let userId: String = UserDefaults.standard.string(forKey: Constants.UserDefaultsKey.userId) else { return }
+        self.loadKikurageUser(uid: userId)
+    }
+}
+
+// MARK: - Firebase Firestore
+extension AppPresenter {
+    /// きくらげユーザーを取得する
+    /// - Parameter uid: ユーザーID
+    func loadKikurageUser(uid: String) {
+        self.kikurageUserRepository.getKikurageUser(uid: uid) { [weak self] response in
+            switch response {
+            case .success(let kikurageUser):
+                self?.delegate?.didSuccessGetKikurageUser(kikurageUser: kikurageUser)
+            case .failure(let error):
+                print(error)
+                self?.delegate?.didFailedGetKikurageUser(errorMessage: "きくらげユーザーを取得できませんでし")
+            }
+        }
+    }
+}
--- kikurageApp/App/AppRootController.swift
+++ kikurageApp/App/AppRootController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class AppRootController: UIViewController {
     private var currentViewController: UIViewController?
+    private var presenter: AppPresenter!
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        showTopPage()
+        self.presenter = AppPresenter(kikurageUserRepository: KikurageUserRepository())
+        self.presenter.delegate = self
+
+        self.presenter.checkSavedUserId()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -25,9 +29,18 @@ class AppRootController: UIViewController {
 // MARK: - Private
 extension AppRootController {
     /// トップ画面を開く
-    private func showTopPage() {
+    private func showTopPage(kikurageUser: KikurageUser) {
+        guard let vc = R.storyboard.mainViewController.instantiateInitialViewController() else { return }
+        let navVC = UINavigationController(rootViewController: vc)
+        let mainVC = navVC.topViewController as! MainViewController // swiftlint:disable:this force_cast
+        mainVC.kikurageUser = kikurageUser
+        changeViewController(mainVC)
+    }
+    /// ログイン画面を開く
+    private func showLoginPage() {
         guard let vc = R.storyboard.topViewController.instantiateInitialViewController() else { return }
-        changeViewController(vc)
+        let navVC = UINavigationController(rootViewController: vc)
+        changeViewController(navVC)
     }
     private func changeViewController(_ vc: UIViewController) {
         removeCurrentViewController()
@@ -43,5 +56,16 @@ extension AppRootController {
         guard let vc = currentViewController else { return }
         vc.willMove(toParent: nil)
         vc.view.removeFromSuperview()
+    }
+}
+
+// MARK: - AppPresenter Delegate
+extension AppRootController: AppPresenterDelegate {
+    func didSuccessGetKikurageUser(kikurageUser: KikurageUser) {
+        showTopPage(kikurageUser: kikurageUser)
+    }
+    func didFailedGetKikurageUser(errorMessage: String) {
+        print("DEBUG: \(errorMessage)")
+        showLoginPage()
     }
 }
