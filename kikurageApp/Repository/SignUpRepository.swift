e702118f652689709a758c828315f6c5020addad
shusuke0812 <shu.ota0812@gmail.com>
2021-09-09 23:40:56 +0900

    add: ユーザー登録リクエスト処理を追加

--- kikurageApp/Repository/SignUpRepository.swift
+++ kikurageApp/Repository/SignUpRepository.swift
@@ -7,10 +7,11 @@
 //
 
 import Foundation
+import Firebase
 
 protocol SignUpRepositoryProtocol {
     /// 新規ユーザー登録を行う
-    func registerUser(registerInfo: (email: String, password: String), completion: @escaping (Result<, Error>) -> Void)
+    func registerUser(registerInfo: (email: String, password: String), completion: @escaping (Result<User, Error>) -> Void)
 }
 
 class SignUpRepository: SignUpRepositoryProtocol {
@@ -18,5 +19,15 @@ class SignUpRepository: SignUpRepositoryProtocol {
 
 // MARK: - Firebase Authentication
 extension SignUpRepository {
-    
+    // TODO: メール認証機能を追加
+    func registerUser(registerInfo: (email: String, password: String), completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: registerInfo.email, password: registerInfo.password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            if let user = authResult?.user {
+                completion(.success(user))
+            }
+        }
+    }
 }
--- kikurageApp/ViewModel/Login/SignUpViewModel.swift
+++ kikurageApp/ViewModel/Login/SignUpViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Firebase
 
 protocol SignUpViewModelDelegate: AnyObject {
     func didSuccessRegisterUser()
@@ -16,7 +17,9 @@ protocol SignUpViewModelDelegate: AnyObject {
 class SignUpViewModel {
     private var signUpRepository: SignUpRepositoryProtocol
     
-    weak var delegate: SignUpBaseViewDelegate?
+    weak var delegate: SignUpViewModelDelegate?
+    
+    private var user: User?
     
     init(signUpRepository: SignUpRepositoryProtocol) {
         self.signUpRepository = signUpRepository
@@ -26,5 +29,14 @@ class SignUpViewModel {
 // MARK: - Firebase Authentication
 extension SignUpViewModel {
     func registerUser(registerInfo: (email: String, password: String)) {
+        self.signUpRepository.registerUser(registerInfo: registerInfo) { [weak self] response in
+            switch response {
+            case .success(let user):
+                self?.user = user
+            case .failure(let error):
+                print("DEBUG: \(error)")
+                self?.delegate?.didFailedRegisterUser(errorMessage: "ユーザー登録に失敗しました")
+            }
+        }
     }
 }
