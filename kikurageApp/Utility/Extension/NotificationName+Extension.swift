56f226f35cb330a2e8a02e14972deb59e15644cb
shusuke0812 <shu.ota0812@gmail.com>
2021-09-13 21:30:39 +0900

    add: Notification.Nameのextensionを追加

---
+++ kikurageApp/Utility/Extension/NotificationName+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationCenter+Extension.swift
+//  kikurageApp
+//
+//  Created by Shusuke Ota on 2021/9/13.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let updatedCultivations  = Notification.Name("UpdatedCultivations")
+    static let updatedRecipes       = Notification.Name("UpdatedRecipes")
+}
