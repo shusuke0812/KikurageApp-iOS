3cbc2006ad69d441839985719731d1c81d26ebb2
shusuke0812 <shu.ota0812@gmail.com>
2021-10-20 09:11:13 +0900

    fix: DateHelper型のformatterを遅延初期化して使うように修正

--- kikurageApp/Utility/Helper/DateHelper.swift
+++ kikurageApp/Utility/Helper/DateHelper.swift
@@ -9,45 +9,40 @@
 import Foundation
 
 struct DateHelper {
-    /// 現在時刻を取得する
-    static func now() -> String {
-        let now = Date()
-
+    private static let defaultDateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .gregorian)
         formatter.locale = Locale(identifier: "ja_JP")
         formatter.dateStyle = .medium
         formatter.timeStyle = .medium
-
-        return formatter.string(from: now)
+        return formatter
+    }()
+    private static let originalDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+    /// 現在時刻を取得する
+    static func now() -> String {
+        let now = Date()
+        return self.defaultDateFormatter.string(from: now)
     }
     /// Date型を指定したString型に変換する
     /// - Parameter date: 日付
     static func formatToString(date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .gregorian)
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-
-        formatter.dateFormat = "yyyy/MM/dd"
-        return formatter.string(from: date)
+        self.originalDateFormatter.dateFormat = "yyyy/MM/dd"
+        return self.originalDateFormatter.string(from: date)
     }
     /// Date型を画像ファイル名に使うString型に変換する
     /// - Parameter date: 日付
     static func formatToStringForImageData(date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .gregorian)
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-
-        formatter.dateFormat = "yyyyMMddHHmmss"
-        return formatter.string(from: date)
+        self.originalDateFormatter.dateFormat = "yyyyMMddHHmmss"
+        return self.originalDateFormatter.string(from: date)
     }
     /// Date型をログに使うString型へ変換する
     static func formatToStringForLog() -> String {
-        let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .gregorian)
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-
-        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
-        return formatter.string(from: Date())
+        self.originalDateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        return self.originalDateFormatter.string(from: Date())
     }
 }
