ce2f64b320b1b2356bc1c586e586a466b29b66cb
shusuke0812 <shu.ota0812@gmail.com>
2021-11-06 13:09:31 +0900

    add: ClientErrorにcaseを追加

--- kikurageApp/Entity/Error/ClientError.swift
+++ kikurageApp/Entity/Error/ClientError.swift
@@ -11,19 +11,25 @@ import Foundation
 enum ClientError: Error {
     /// 通信に失敗（ex. 端末オフライン、URLホストが見つからない etc）
     case networkConnectionError(Error)
+    /// エンコード、デコードに失敗
+    case parseError(Error)
     /// レスポンスの変換に失敗（ex. レスポンスのJSON形式とResonse型がアンマッチ、JSONデータが一部欠けていた etc）
     case responseParseError(Error)
     /// APIからのエラーレスポンス（400-500番台）
     case apiError(FirebaseAPIError)
     /// 不明なエラー
     case unknown
-    
+    /// FirestoreのドキュメントIDが見つからない
+    case documentIdError
+
     func description() -> String {
         switch self {
         case .networkConnectionError:   return "ネットワーク通信に失敗しました"
+        case .parseError:               return "パースエラー"
         case .responseParseError:       return "レスポンスの変換に失敗しました"
         case .apiError:                 return "サーバーエラー"
         case .unknown:                  return "エラーが発生しました"
+        case .documentIdError:          return "ドキュメントIDが見つかりません"
         }
     }
 }
