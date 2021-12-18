21d0ccd63ec7147e35dbd1181442af4e379c41d0
shusuke0812 <shu.ota0812@gmail.com>
2021-12-18 21:05:28 +0900

    add: HomeVM、テストファイルとスタブファイルを追加

---
+++ KikurageTests/Repository/StubKikurageStateRepository.swift
@@ -0,0 +1,9 @@
+//
+//  StubKikurageStateRepository.swift
+//  KikurageTests
+//
+//  Created by Shusuke Ota on 2021/12/18.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import Foundation
