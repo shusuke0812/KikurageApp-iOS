50dc7cd0da5379ce7ef98a52a1dff120680e5c44
shusuke0812 <shu.ota0812@gmail.com>
2021-12-25 13:03:31 +0900

    add: ホーム画面、下部に設置するカスタムViewを追加

---
+++ Kikurage/ScreenModule/Home/ChildView/FooterButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  FooterButtonView.swift
+//  Kikurage
+//
+//  Created by Shusuke Ota on 2021/12/25.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class FooterButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
