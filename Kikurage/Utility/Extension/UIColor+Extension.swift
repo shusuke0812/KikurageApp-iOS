d530e5742a8252d785b1e6f2ed8a59d619985590
shusuke0812 <shu.ota0812@gmail.com>
2021-12-25 15:59:08 +0900

    add: UIColorのextensionに16進数文字列でカラーを初期化できるイニシャライザを追加

--- Kikurage/Utility/Extension/UIColor+Extension.swift
+++ Kikurage/Utility/Extension/UIColor+Extension.swift
@@ -21,4 +21,12 @@ extension UIColor {
     static var information: UIColor {
         UIColor(named: "informationColor")! // swiftlint:disable:this force_unwrapping
     }
+    /// HEX（16進数）で色を設定する
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let value = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(value / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(value / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(value / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+    }
 }
