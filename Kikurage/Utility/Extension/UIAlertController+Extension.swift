ab5c0ed32c769f27854f803a9df610f225582226
shusuke0812 <shu.ota0812@gmail.com>
2021-12-28 01:25:32 +0900

    add: アラートのボタンを押したらコンソールにログを出力するようにした
    ・エラー、デバッグ対策

--- Kikurage/Utility/Extension/UIAlertController+Extension.swift
+++ Kikurage/Utility/Extension/UIAlertController+Extension.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 extension UIAlertController {
-    static func showAlert(style: UIAlertController.Style, viewController: UIViewController, title: String?, message: String?, okButtonTitle: String, cancelButtonTitle: String?, completionOk: (() -> Void)?) {
+    static func showAlert(style: UIAlertController.Style, viewController: UIViewController, title: String?, message: String?, okButtonTitle: String, cancelButtonTitle: String?, completionOk: (() -> Void)?,
+                          file: String = #file, function: String = #function, line: Int = #line, logMessage: String = "") {
         let alert = UIAlertController(title: title, message: message, preferredStyle: style)
         // キャンセル｜OK というボタン配置にするためにCancelActionを先にAddする
         if let cancelButtonTitle = cancelButtonTitle {
             let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ -> Void in
-                print("DEBUG: アラートのキャンセルボタンが押されました")
+                Logger.info(file: file, function: function, line: line, logMessage)
             }
             alert.addAction(cancelAction)
         }
         let okAction = UIAlertAction(title: okButtonTitle, style: .default) { _ -> Void in
-            print("DEBUG: アラートのOKボタンが押されました")
+            Logger.info(file: file, function: function, line: line, logMessage)
             if let completionOk = completionOk {
                 completionOk()
             }
