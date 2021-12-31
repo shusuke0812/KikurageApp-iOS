275204ae8c4ff7746c578df6f9bd672d3e3f3060
shusuke0812 <shu.ota0812@gmail.com>
2021-12-31 17:02:26 +0900

    add: サイドメニュー、カレンダーのViewを追加した

--- Kikurage/ScreenModule/SideMenu/Child/Calendar/BaseView/CalendarBaseView.swift
+++ Kikurage/ScreenModule/SideMenu/Child/Calendar/BaseView/CalendarBaseView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import HorizonCalendar
 
 protocol CalendarBaseViewDelegate: AnyObject {
     /// 閉じるボタンを押した時の処理
@@ -15,12 +16,14 @@ protocol CalendarBaseViewDelegate: AnyObject {
 
 class CalendarBaseView: UIView {
     @IBOutlet private weak var navigationItem: UINavigationItem!
+    @IBOutlet private weak var contentView: UIView!
 
     weak var delegate: CalendarBaseViewDelegate?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         initUI()
+        initCalendarView()
     }
     
     // MARK: - Action
@@ -34,7 +37,45 @@ class CalendarBaseView: UIView {
 
 extension CalendarBaseView {
     private func initUI() {
-        backgroundColor = .systemGroupedBackground
+        contentView.backgroundColor = .systemGroupedBackground
         navigationItem.title = R.string.localizable.side_menu_clendar_title()
+    }
+    private func initCalendarView() {
+        let parentView = UIView()
+        parentView.backgroundColor = .white
+        parentView.clipsToBounds = true
+        parentView.layer.cornerRadius = .viewCornerRadius
+        parentView.translatesAutoresizingMaskIntoConstraints = false
+
+        let calendarView = CalendarView(initialContent: makeContent())
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        parentView.addSubview(calendarView)
+        contentView.addSubview(parentView)
+
+        let contentViewWidth = UIScreen.main.bounds.size.width - (15 * 2)
+
+        NSLayoutConstraint.activate([
+            parentView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 50),
+            parentView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            parentView.widthAnchor.constraint(equalToConstant: contentViewWidth),
+            parentView.heightAnchor.constraint(equalToConstant: contentViewWidth),
+            
+            calendarView.topAnchor.constraint(equalTo: parentView.topAnchor, constant: 15),
+            calendarView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor, constant: 15),
+            calendarView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor, constant: -15),
+            calendarView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor, constant: -15)
+        ])
+    }
+    private func makeContent() -> CalendarViewContent {
+        let calendar = Calendar.current
+        let startDate = calendar.date(from: DateComponents(year: 2021, month: 01, day: 01))!
+        let endDate = calendar.date(from: DateComponents(year: 2021, month: 12, day: 31))!
+        
+        return CalendarViewContent(
+            calendar: calendar,
+            visibleDateRange: startDate...endDate,
+            monthsLayout: .horizontal(options: HorizontalMonthsLayoutOptions())
+        )
     }
 }
