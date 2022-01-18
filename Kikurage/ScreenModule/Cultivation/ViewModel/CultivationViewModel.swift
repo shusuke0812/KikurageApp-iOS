efe38b04b0bd59ad4fd02ea9727a8f1163eb3bb9
shusuke0812 <shu.ota0812@gmail.com>
2022-01-18 23:55:06 +0900

    fix: 栽培記録一覧画面、記録日の降順で表示されるようにした

--- Kikurage/ScreenModule/Cultivation/ViewModel/CultivationViewModel.swift
+++ Kikurage/ScreenModule/Cultivation/ViewModel/CultivationViewModel.swift
@@ -39,14 +39,12 @@ class CultivationViewModel: CultivationViewModelType, CultivationViewModelInput,
 // MARK: - Config
 
 extension CultivationViewModel {
-    private func sortCultivations() {
-        /* For Not Observable object
-        cultivations.sort { cultivation1, cultivation2 -> Bool in
+    private func sortCultivations(cultivations: [(cultivation: KikurageCultivation, documentId: String)]) -> [(cultivation: KikurageCultivation, documentId: String)] {
+        cultivations.sorted { cultivation1, cultivation2 -> Bool in
             guard let cultivationDate1 = DateHelper.formatToDate(dateString: cultivation1.cultivation.viewDate) else { return false }
             guard let cultivationDate2 = DateHelper.formatToDate(dateString: cultivation2.cultivation.viewDate) else { return false }
             return cultivationDate1 > cultivationDate2
         }
-        */
     }
 }
 
@@ -56,14 +54,18 @@ extension CultivationViewModel {
     /// きくらげ栽培記録を読み込む
     func loadCultivations(kikurageUserId: String) {
         cultivationRepository.getCultivations(kikurageUserId: kikurageUserId) { [weak self] response in
+            guard let `self` = self else {
+                self?.subject.onError(ClientError.unknown)
+                return
+            }
             switch response {
             case .success(let cultivations):
                 Logger.verbose("\(cultivations)")
-                // TODO: 降順にソートさせる
-                self?.subject.onNext(cultivations)
+                let cultivations = self.sortCultivations(cultivations: cultivations)
+                self.subject.onNext(cultivations)
             case .failure(let error):
                 Logger.verbose(error.localizedDescription)
-                self?.subject.onError(error)
+                self.subject.onError(error)
             }
         }
     }
