9851e4da32c77f4b15eb1d2fd5b8ae8c1f853bc2
shusuke0812 <shu.ota0812@gmail.com>
2022-02-11 23:56:48 +0900

    fix: Podfile修正

--- KikurageUITests/LoginUITests.swift
+++ KikurageUITests/LoginUITests.swift
@@ -1,8 +1,8 @@
 //
 //  LoginUITests.swift
-//  LoginUITests
+//  KikurageUITests
 //
-//  Created by Shusuke Ota on 2022/2/8.
+//  Created by Shusuke Ota on 2022/2/11.
 //  Copyright © 2022 shusuke. All rights reserved.
 //
 
@@ -16,6 +16,9 @@ class LoginUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -24,20 +27,8 @@ class LoginUITests: XCTestCase {
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
