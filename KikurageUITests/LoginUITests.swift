734ce58a18727aaf059a7a1952faa2926a9bcff8
shusuke0812 <shu.ota0812@gmail.com>
2022-02-12 23:20:21 +0900

    add: UITest、ログイン用のPageObjectを追加

--- KikurageUITests/LoginUITests.swift
+++ KikurageUITests/LoginUITests.swift
@@ -33,29 +33,7 @@ class LoginUITests: XCTestCase {
 
 extension LoginUITests {
     private func testLogin() {
-        // FIXME: AccessibilityIDの設定方法を考える（Kikurageモジュールで定義したManagerは使えない、理由は不明）
-        let button = app.buttons["TopBaseView_login_button"]
-        button.tap()
-
-        let emailTextField = app.textFields["LoginBaseView_email_textfield"]
-        emailTextField.tap()
-        emailTextField.typeText(createUniqueEmail())
-
-        let passwordTextField = app.secureTextFields["LoginBaseView_password_textfield"]
-        passwordTextField.tap()
-        passwordTextField.typeText("test123")
-
-        let loginButton = app.buttons["LoginBaseView_login_button"]
-        loginButton.tap()
-    }
-}
-
-// MARK: - Stub
-
-extension LoginUITests {
-    private func createUniqueEmail() -> String {
-        //"test+\(Date().timeIntervalSince1970)@kikurage.com"
-        let num = Int.random(in: 1..<200)
-        return "test\(num)@kikurage.com"
+        let loginPage = TopPage().goToLogin()
+        loginPage.login()
     }
 }
--- KikurageUITests/Page/PageObjectable.swift
+++ KikurageUITests/Page/PageObjectable.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2022 shusuke. All rights reserved.
 //
 
-import Foundation
 import XCTest
 
 protocol PageObjectable {
