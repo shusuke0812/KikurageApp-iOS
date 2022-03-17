f3cf6e8308151a96073aac410750320250be1003
shusuke0812 <shu.ota0812@gmail.com>
2022-03-18 00:01:48 +0900

    add: RecipeRepositoryにRxメソッドを追加

--- Kikurage/Repository/RecipeRepository.swift
+++ Kikurage/Repository/RecipeRepository.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import RxSwift
 
 protocol RecipeRepositoryProtocol {
     /// 料理記録を投稿する
@@ -16,12 +17,14 @@ protocol RecipeRepositoryProtocol {
     ///   - kikurageRecipe: Firestoreへ保存する料理記録データ
     ///   - completion: 投稿成功、失敗のハンドル
     func postRecipe(kikurageUserId: String, kikurageRecipe: KikurageRecipe, completion: @escaping (Result<DocumentReference, ClientError>) -> Void)
+    func postRecipe(kikurageUserId: String, kikurageRecipe: KikurageRecipe) -> Single<DocumentReference>
     /// 料理画像を保存する（直列処理）
     /// - Parameters:
     ///   - imageData: 保存する画像データ
     ///   - imageStoragePath: 画像を保存するStorageパス
     ///   - completion: 投稿成功、失敗のハンドル
     func postRecipeImages(imageData: [Data?], imageStoragePath: String, completion: @escaping (Result<[String], ClientError>) -> Void)
+    func postRecipeImages(imageData: [Data?], imageStoragePath: String) -> Single<[String]>
     /// 栽培画像のStoragePathを更新する
     /// - Parameters:
     ///   - kikurageUserId: ユーザーID
@@ -29,11 +32,13 @@ protocol RecipeRepositoryProtocol {
     ///   - imageStorageFullPaths: Storageに保存した画像のフルパス
     ///   - completion: 投稿成功、失敗のハンドル
     func putRecipeImage(kikurageUserId: String, documentId: String, imageStorageFullPaths: [String], completion: @escaping (Result<[String], ClientError>) -> Void)
+    func putRecipeImage(kikurageUserId: String, documentId: String, imageStorageFullPaths: [String]) -> Single<[String]>
     /// 栽培記録を取得する
     /// - Parameters:
     ///   - kikurageUserId: ユーザーID
     ///   - completion: 投稿成功、失敗のハンドル
     func getRecipes(kikurageUserId: String, completion: @escaping (Result<[(recipe: KikurageRecipe, documentId: String)], ClientError>) -> Void)
+    func getRecipes(kikurageUserId: String) -> Single<[(recipe: KikurageRecipe, documentId: String)]>
 }
 
 class RecipeRepository: RecipeRepositoryProtocol {
@@ -71,6 +76,30 @@ extension RecipeRepository {
             completion(.success(documentReference))
         }
     }
+    func postRecipe(kikurageUserId: String, kikurageRecipe: KikurageRecipe) -> Single<DocumentReference> {
+        Single<DocumentReference>.create { single in
+            let db = Firestore.firestore()
+            var data: [String: Any]!
+            do {
+                data = try Firestore.Encoder().encode(kikurageRecipe)
+            } catch {
+                single(.failure(ClientError.parseError(error)))
+            }
+            let dispatchGroup = DispatchGroup()
+            dispatchGroup.enter()
+            let documentReference: DocumentReference = db.collection(Constants.FirestoreCollectionName.users).document(kikurageUserId).collection(Constants.FirestoreCollectionName.recipes).addDocument(data: data) { error in
+                if let error = error {
+                    dump(error)
+                    single(.failure(ClientError.apiError(.createError)))
+                }
+                dispatchGroup.leave()
+            }
+            dispatchGroup.notify(queue: .main) {
+                single(.success(documentReference))
+            }
+            return Disposables.create()
+        }
+    }
     func putRecipeImage(kikurageUserId: String, documentId: String, imageStorageFullPaths: [String], completion: @escaping (Result<[String], ClientError>) -> Void) {
         let db = Firestore.firestore()
         let documentReference = db.collection(Constants.FirestoreCollectionName.users).document(kikurageUserId).collection(Constants.FirestoreCollectionName.recipes).document(documentId)
@@ -83,6 +112,23 @@ extension RecipeRepository {
             } else {
                 completion(.success(imageStorageFullPaths))
             }
+        }
+    }
+    func putRecipeImage(kikurageUserId: String, documentId: String, imageStorageFullPaths: [String]) -> Single<[String]> {
+        Single<[String]>.create { single in
+            let db = Firestore.firestore()
+            let documentReference = db.collection(Constants.FirestoreCollectionName.users).document(kikurageUserId).collection(Constants.FirestoreCollectionName.recipes).document(documentId)
+            documentReference.updateData([
+                "imageStoragePaths": imageStorageFullPaths
+            ]) { error in
+                if let error = error {
+                    dump(error)
+                    single(.failure(ClientError.apiError(.updateError)))
+                } else {
+                    single(.success(imageStorageFullPaths))
+                }
+            }
+            return Disposables.create()
         }
     }
     func getRecipes(kikurageUserId: String, completion: @escaping (Result<[(recipe: KikurageRecipe, documentId: String)], ClientError>) -> Void) {
@@ -108,6 +154,34 @@ extension RecipeRepository {
             } catch {
                 completion(.failure(ClientError.responseParseError(error)))
             }
+        }
+    }
+    func getRecipes(kikurageUserId: String) -> Single<[(recipe: KikurageRecipe, documentId: String)]> {
+        Single<[(recipe: KikurageRecipe, documentId: String)]>.create { single in
+            let db = Firestore.firestore()
+            let collectionReference = db.collection(Constants.FirestoreCollectionName.users).document(kikurageUserId).collection(Constants.FirestoreCollectionName.recipes)
+            collectionReference.getDocuments { snapshot, error in
+                if let error = error {
+                    dump(error)
+                    single(.failure(ClientError.apiError(.readError)))
+                    return
+                }
+                guard let snapshot = snapshot else {
+                    single(.failure(ClientError.apiError(.readError)))
+                    return
+                }
+                var recipes: [(recipe: KikurageRecipe, documentId: String)] = []
+                do {
+                    for document in snapshot.documents {
+                        let recipe = try Firestore.Decoder().decode(KikurageRecipe.self, from: document.data())
+                        recipes.append((recipe: recipe, documentId: document.documentID))
+                    }
+                    single(.success(recipes))
+                } catch {
+                    single(.failure(ClientError.responseParseError(error)))
+                }
+            }
+            return Disposables.create()
         }
     }
 }
@@ -150,6 +224,41 @@ extension RecipeRepository {
                     completion(.success(imageStorageFullPaths))
                 }
             }
+        }
+    }
+    func postRecipeImages(imageData: [Data?], imageStoragePath: String) -> Single<[String]> {
+        Single<[String]>.create { single in
+            var imageStorageFullPaths: [String] = []
+            let dispatchSemaphore = DispatchSemaphore(value: 0)
+            let dispatchQueue = DispatchQueue(label: "com.shusuke.KikurageApp.upload_recipe_images_queue")
+            var resultError: Error?
+            dispatchQueue.async {
+                for (i, imageData) in zip(imageData.indices, imageData) {
+                    guard let imageData = imageData else {
+                        dispatchSemaphore.signal()
+                        return
+                    }
+                    let fileName: String = DateHelper.formatToStringForImageData(date: Date()) + "_\(i).jpeg"
+                    let storageReference = Storage.storage().reference().child(imageStoragePath + fileName)
+                    _ = storageReference.putData(imageData, metadata: self.metaData) { _, error in
+                        if let error = error {
+                            resultError = error
+                            dispatchSemaphore.signal()
+                            return
+                        }
+                        imageStorageFullPaths.append(storageReference.fullPath)
+                        dispatchSemaphore.signal()
+                    }
+                    dispatchSemaphore.wait()
+                }
+                if let resultError = resultError {
+                    dump(resultError)
+                    single(.failure(ClientError.apiError(.createError)))
+                } else {
+                    single(.success(imageStorageFullPaths))
+                }
+            }
+            return Disposables.create()
         }
     }
 }
