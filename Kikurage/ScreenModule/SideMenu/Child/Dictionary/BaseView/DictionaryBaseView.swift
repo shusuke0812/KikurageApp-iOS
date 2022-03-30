aedcb00a15bbfe38e71af263eedbd290182569b0
shusuke0812 <shu.ota0812@gmail.com>
2022-03-30 22:47:38 +0900

    add: 豆知識画面、VC / Storyboard / VM / BaseView ファイルを追加

---
+++ Kikurage/ScreenModule/SideMenu/Child/Dictionary/BaseView/DictionaryBaseView.swift
@@ -0,0 +1,15 @@
+//
+//  DictionaryBaseView.swift
+//  Kikurage
+//
+//  Created by Shusuke Ota on 2022/3/30.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class DictionaryBaseView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ Kikurage/ScreenModule/SideMenu/Child/Dictionary/ViewController/DictionaryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryViewController.swift
+//  Kikurage
+//
+//  Created by Shusuke Ota on 2022/3/30.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class DictionaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
