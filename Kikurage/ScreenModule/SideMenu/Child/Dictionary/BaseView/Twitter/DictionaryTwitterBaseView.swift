ee4935be18014f510d0046d28de06742a7c88e6e
shusuke0812 <shu.ota0812@gmail.com>
2022-04-02 23:57:49 +0900

    add: In SideMenu Dictionary, add twitter view files

--- Kikurage/ScreenModule/SideMenu/Child/Dictionary/BaseView/Twitter/DictionaryTwitterBaseView.swift
+++ Kikurage/ScreenModule/SideMenu/Child/Dictionary/BaseView/Twitter/DictionaryTwitterBaseView.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class DictionaryTwitterBaseView: UIView {
+    @IBOutlet private weak var tableView: UITableView!
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
     }
-    */
+}
 
+// MARK: - Config
+
+extension DictionaryTwitterBaseView {
+    func configTableView(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
 }
--- Kikurage/ScreenModule/SideMenu/Child/Dictionary/ViewController/Twitter/DictionaryTwitterViewController.swift
+++ Kikurage/ScreenModule/SideMenu/Child/Dictionary/ViewController/Twitter/DictionaryTwitterViewController.swift
@@ -9,22 +9,29 @@
 import UIKit
 
 class DictionaryTwitterViewController: UIViewController {
+    private var baseView: DictionaryTwitterBaseView { self.view as! DictionaryTwitterBaseView } // swiftlint:disable:this force_cast
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        baseView.configTableView(delegate: self, dataSource: self)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - UITableViewDelegate
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension DictionaryTwitterViewController: UITableViewDelegate {
+}
+
+// MARK: - UITableViewDataSource
+
+extension DictionaryTwitterViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
     }
-    */
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
 }
