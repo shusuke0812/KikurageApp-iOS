68dfe73648e82257560aa0c30c244f84aca595df
shusuke0812 <shu.ota0812@gmail.com>
2022-04-02 23:57:39 +0900

    fix

---
+++ Kikurage/ScreenModule/SideMenu/Child/Dictionary/BaseView/Twitter/DictionaryTwitterBaseView.swift
@@ -0,0 +1,21 @@
+//
+//  DictionaryTwitterBaseView.swift
+//  Kikurage
+//
+//  Created by Shusuke Ota on 2022/4/2.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class DictionaryTwitterBaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
