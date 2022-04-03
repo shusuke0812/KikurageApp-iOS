fa9fc76ae6399ee4a78623fdfeb318b1533c7977
shusuke0812 <shu.ota0812@gmail.com>
2022-04-03 23:28:09 +0900

    fix: inplement http request method in APIClient type

--- Kikurage/Repository/APIClient.swift
+++ Kikurage/Repository/APIClient.swift
@@ -14,5 +14,33 @@ protocol APIClientProtocol {
 
 struct APIClient: APIClientProtocol {
     func sendRequest<T: APIRequestProtocol>(_ request: T, completion: @escaping (Result<T.Response, T.ErrorResponse>) -> Void) {
+        let session = URLSession.shared
+        let errorResponse = T.ErrorResponse.self as! T.ErrorResponse
+        let task = session.dataTask(with: request.buildUrlRequest()) { data, response, error in
+            if let error = error {
+                KLogger.debug(error.localizedDescription)
+                completion(.failure(errorResponse))
+                return
+            }
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                completion(.failure(errorResponse))
+                return
+            }
+            // TODO: write decode method outside this type
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            if (200 ..< 300).contains(response.statusCode) {
+                do {
+                    let apiResponse = try decoder.decode(T.Response.self, from: data)
+                    completion(.success(apiResponse))
+                } catch {
+                    completion(.failure(errorResponse))
+                }
+            } else {
+                completion(.failure(errorResponse))
+            }
+        }
+        task.resume()
     }
 }
