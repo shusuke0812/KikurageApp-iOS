069fa7527373a29f34c2658f7730230e553e4534
shusuke0812 <shu.ota0812@gmail.com>
2022-04-03 21:11:09 +0900

    add: request building method in APIRequestProtocol

--- Kikurage/Entity/Request/APIRequestProtocol.swift
+++ Kikurage/Entity/Request/APIRequestProtocol.swift
@@ -17,6 +17,30 @@ protocol APIRequestProtocol {
     var parameters: [URLQueryItem]? { get }
     var header: [String: String]? { get }
     var body: Data? { get }
+    
+    func buildUrlRequest() -> URLRequest
+}
+
+extension APIRequestProtocol {
+    func buildUrlRequest() -> URLRequest {
+        let url = URL(string: baseUrl.appending(path))! // swiftlint:disable:this force_unwrapping
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        switch method {
+        case .get:
+            components?.queryItems = parameters
+        default:
+            fatalError("this is not supported http method: \(method)")
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.url = components?.url
+        urlRequest.httpMethod = method.rawValue
+        header?.forEach { key, value in
+            urlRequest.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        return urlRequest
+    }
 }
 
 enum HTTPMethod: String {
