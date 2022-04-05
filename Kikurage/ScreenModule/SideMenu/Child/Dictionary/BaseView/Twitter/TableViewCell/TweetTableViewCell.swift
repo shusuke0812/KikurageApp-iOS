017ea28c2a19376bcf45fa75d087253236aee936
shusuke0812 <shu.ota0812@gmail.com>
2022-04-05 23:34:59 +0900

    add: TweetCell, add setting UI method

--- Kikurage/ScreenModule/SideMenu/Child/Dictionary/BaseView/Twitter/TableViewCell/TweetTableViewCell.swift
+++ Kikurage/ScreenModule/SideMenu/Child/Dictionary/BaseView/Twitter/TableViewCell/TweetTableViewCell.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import SDWebImage
 
 class TweetTableViewCell: UITableViewCell {
     @IBOutlet private weak var userIconImageView: UIImageView!
     @IBOutlet private weak var userNameLabel: UILabel!
     @IBOutlet private weak var createdAtLabel: UILabel!
     @IBOutlet private weak var tweetLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        initUI()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -31,9 +33,9 @@ extension TweetTableViewCell {
         userIconImageView.layer.cornerRadius = userIconImageView.frame.width / 2
         userIconImageView.layer.borderColor = UIColor.lightGray.cgColor
         userIconImageView.layer.borderWidth = 0.3
-        
+
         userNameLabel.font = .boldSystemFont(ofSize: 17.0)
-        
+
         tweetLabel.font = .systemFont(ofSize: 17.0, weight: .light)
     }
 }
@@ -41,6 +43,12 @@ extension TweetTableViewCell {
 // MARK: - Setting UI
 
 extension TweetTableViewCell {
-    func setUI() {
+    func setUI(tweet: Tweet.Status) {
+        userNameLabel.text = tweet.user.name
+        createdAtLabel.text = DateHelper.formatToString(date: tweet.createdAt)
+        tweetLabel.text = tweet.text
+        if let iconImageUrlString = URL(string: tweet.user.profileImageUrl) {
+            userIconImageView.sd_setImage(with: iconImageUrlString)
+        }
     }
 }
