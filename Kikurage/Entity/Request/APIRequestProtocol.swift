18f54bbe35036b1a97d5ebea293e96e5279dc291
shusuke0812 <shu.ota0812@gmail.com>
2022-04-09 20:43:23 +0900

    fix: separated HTTPMethod type

--- Kikurage/Entity/Request/APIRequestProtocol.swift
+++ Kikurage/Entity/Request/APIRequestProtocol.swift
@@ -43,10 +43,3 @@ extension APIRequestProtocol {
         return urlRequest
     }
 }
-
-enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
-}
