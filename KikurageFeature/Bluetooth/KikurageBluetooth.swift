d5e7305f0414e41653871b4f5733482666f44170
shusuke0812 <shu.ota0812@gmail.com>
2022-04-22 23:34:26 +0900

    add: In KikurageFeature, add files of Bluetooth method

---
+++ KikurageFeature/Bluetooth/KikurageBluetooth.swift
@@ -0,0 +1,12 @@
+//
+//  KikurageBluetooth.swift
+//  KikurageFeature
+//
+//  Created by Shusuke Ota on 2022/4/22.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import Foundation
+
+public class KikurageBluetooth {
+}
