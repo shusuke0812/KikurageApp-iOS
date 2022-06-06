f6be26de48d93fde869a71e522ade31b2b0e25b0
shusuke0812 <shu.ota0812@gmail.com>
2022-06-06 23:57:01 +0900

    add: files for debug mode view

---
+++ Kikurage/ScreenModule/SideMenu/Child/Debug/BaseView/DebugBaseView.swift
@@ -0,0 +1,21 @@
+//
+//  DebugBaseView.swift
+//  Kikurage
+//
+//  Created by Shusuke Ota on 2022/6/6.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class DebugBaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Kikurage/ScreenModule/SideMenu/Child/Debug/ViewModel/DebugViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DebugViewModel.swift
+//  Kikurage
+//
+//  Created by Shusuke Ota on 2022/6/6.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import Foundation
