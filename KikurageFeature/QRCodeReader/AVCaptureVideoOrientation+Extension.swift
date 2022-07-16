a79d657bd146b4d2f892929145ff4b912efcdd69
shusuke0812 <shu.ota0812@gmail.com>
2022-07-16 20:24:15 +0900

    add: AVCaptureVideoOrientation+Extension.swift

---
+++ KikurageFeature/QRCodeReader/AVCaptureVideoOrientation+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  AVCaptureVideoOrientation+Extension.swift
+//  KikurageFeature
+//
+//  Created by Shusuke Ota on 2022/7/16.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import UIKit
+
+extension AVCaptureVideoOrientation {
+    init?(deviceOrientation: UIDeviceOrientation) {
+        switch deviceOrientation {
+        case .portrait: self = .portrait
+        case .portraitUpsideDown: self = .portraitUpsideDown
+        case .landscapeLeft: self = .landscapeRight
+        case .landscapeRight: self = .landscapeLeft
+        default: return nil
+        }
+    }
+    init?(interfaceOrientation: UIInterfaceOrientation) {
+        switch interfaceOrientation {
+        case .portrait: self = .portrait
+        case .portraitUpsideDown: self = .portraitUpsideDown
+        case .landscapeLeft: self = .landscapeLeft
+        case .landscapeRight: self = .landscapeRight
+        default: return nil
+        }
+    }
+}
