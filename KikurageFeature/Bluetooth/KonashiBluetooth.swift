811aee1c08ade4b675fdfb8a69e340a70c7d04b9
shusuke0812 <shu.ota0812@gmail.com>
2022-07-26 01:24:19 +0900

    fix: KonashiBluetooth dinit

--- KikurageFeature/Bluetooth/KonashiBluetooth.swift
+++ KikurageFeature/Bluetooth/KonashiBluetooth.swift
@@ -26,6 +26,7 @@ public class KonashiBluetooth: NSObject {
 
     deinit {
         readRSSITimer = nil
+        Konashi.shared().signalStrengthDidUpdateHandler = nil
     }
 
     public func find() {
