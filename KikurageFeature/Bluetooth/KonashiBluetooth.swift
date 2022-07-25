51318de90570b6f126c5c55388a77cd39a805352
shusuke0812 <shu.ota0812@gmail.com>
2022-07-26 01:04:33 +0900

    add: method to get konashi RSSI

--- KikurageFeature/Bluetooth/KonashiBluetooth.swift
+++ KikurageFeature/Bluetooth/KonashiBluetooth.swift
@@ -10,14 +10,32 @@ import Foundation
 import konashi_ios_sdk
 
 public class KonashiBluetooth: NSObject {
-    public override init() {
+    private var readRSSITimer: Timer?
+
+    override public init() {
         Konashi.shared().readyHandler = { () -> Void in
             Konashi.pinMode(KonashiDigitalIOPin.LED2, mode: KonashiPinMode.output)
             Konashi.digitalWrite(KonashiDigitalIOPin.LED2, value: KonashiLevel.high)
         }
     }
-    
+
+    deinit {
+        readRSSITimer = nil
+    }
+
     public func find() {
         Konashi.find()
+    }
+
+    // MARK: - RSSI
+    public func readRSSI() {
+        // read request
+        readRSSITimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            Konashi.signalStrengthReadRequest()
+        }
+        // read completion observer
+        Konashi.shared().signalStrengthDidUpdateHandler = { rssi in
+            print(rssi)
+        }
     }
 }
