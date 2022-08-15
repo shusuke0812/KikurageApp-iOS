ed8c362c264648196fb9ddd2e19ccb7a4d7e295a
shusuke0812 <shu.ota0812@gmail.com>
2022-08-16 00:05:05 +0900

    fix: RealmClient

--- Kikurage/Repository/APIClient/RealmClient.swift
+++ Kikurage/Repository/APIClient/RealmClient.swift
@@ -7,7 +7,33 @@
 //
 
 import Foundation
+import RealmSwift
 
 protocol RealmClientProtocol {
-    func writeRequest<T>(_ request: T, completion: @escaping (Result<Void, Error>) -> Void)
+    func writeRequest<T: RealmSwift.Object>(_ object: T, completion: @escaping (Result<Void, Error>) -> Void)
+    func readRequest<T: RealmSwift.Object>(_ object: T, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+struct RealmClient: RealmClientProtocol {
+    func writeRequest<T: RealmSwift.Object>(_ object: T, completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(object)
+                completion(.success(()))
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    func readRequest<T: RealmSwift.Object>(_ object: T, completion: @escaping (Result<T, Error>) -> Void) {
+        do {
+            let realm = try Realm()
+            if let cache = realm.object(ofType: T.self, forPrimaryKey: T.primaryKey()) {
+                completion(.success(cache))
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
 }
