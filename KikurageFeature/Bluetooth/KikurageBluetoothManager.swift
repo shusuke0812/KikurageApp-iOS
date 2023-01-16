36321ff8bbf7eb387457a7edfbe8e2bd44844c65
shusuke0812 <shu.ota0812@gmail.com>
2023-01-16 22:55:35 +0900

    add: scanning peripherals in KikurageBluetoothManager

--- KikurageFeature/Bluetooth/KikurageBluetoothManager.swift
+++ KikurageFeature/Bluetooth/KikurageBluetoothManager.swift
@@ -21,9 +21,30 @@ public class KikurageBluetoothManager: NSObject {
     private func initialize() {
         centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
     }
+    
+    private func scanForPeripherals() {
+        // TODO: setting original service ID
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
 }
 
 extension KikurageBluetoothManager: CBCentralManagerDelegate {
     public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOn:
+            print("DEBUG")
+        case .poweredOff:
+            print("DEBUG")
+        case .unknown:
+            print("DEBUG")
+        case .resetting:
+            print("DEBUG")
+        case .unsupported:
+            print("DEBUG")
+        case .unauthorized:
+            print("DEBUG")
+        @unknown default:
+            fatalError()
+        }
     }
 }
