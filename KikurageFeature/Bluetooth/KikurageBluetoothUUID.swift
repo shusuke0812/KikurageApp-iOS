3a76bc7529557a24433e93e2ea0ab8cb7a00616f
shusuke0812 <shu.ota0812@gmail.com>
2023-03-05 14:59:17 +0900

    fix: KikurageBluetoothUUID

--- KikurageFeature/Bluetooth/KikurageBluetoothUUID.swift
+++ KikurageFeature/Bluetooth/KikurageBluetoothUUID.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+public enum KikurageBluetoothUUID {
+    public enum LocalName {
+        static let debugM5Stack = "kikurage-device-m5-stack"
+    }
+
+    public enum Service {
+        static let debugM5Stack = "65609901-b6ed-45cc-b8af-b4055a9b7666"
+    }
+
+    public enum Characteristic {
+        static let debugM5StackDisplay = "65609902-b6ed-45cc-b8af-b4055a9b7666"
+        static let debugM5Stack9AxisX = "65609903-b6ed-45cc-b8af-b4055a9b7666"
+        static let debugM5Stack9AxisY = "65609904-b6ed-45cc-b8af-b4055a9b7666"
+        static let debugM5Stack9AxisZ = "65609905-b6ed-45cc-b8af-b4055a9b7666"
+    }
+}
