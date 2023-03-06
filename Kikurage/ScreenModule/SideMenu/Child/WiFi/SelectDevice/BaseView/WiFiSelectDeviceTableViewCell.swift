8a92f71d8bd9c5ce42777f96b23b654e6b79b476
shusuke0812 <shu.ota0812@gmail.com>
2023-03-06 16:47:28 +0900

    fix: WiFiSelectDeviceTableViewCell
    add signalImage, signalLabel and bleServiceNumLabel.
    fix constraints of component.

--- Kikurage/ScreenModule/SideMenu/Child/WiFi/SelectDevice/BaseView/WiFiSelectDeviceTableViewCell.swift
+++ Kikurage/ScreenModule/SideMenu/Child/WiFi/SelectDevice/BaseView/WiFiSelectDeviceTableViewCell.swift
@@ -7,41 +7,11 @@
 //
 
 import UIKit
-
-private enum BLESignal {
-    case lost
-    case weak
-    case fair
-    case good
-
-    var image: UIImage? {
-        switch self {
-        case .lost:
-            return R.image.signalLost()
-        case .weak:
-            return R.image.signalWeak()
-        case .fair:
-            return R.image.signalFair()
-        case .good:
-            return R.image.signalGood()
-        }
-    }
-    
-    // NOTE: Based on experiment value using LightBlue App.
-    func getSignal(rssi: Int) -> Self {
-        if rssi < 45 {
-            return .good
-        } else if rssi < 60 {
-            return .fair
-        } else if rssi < 80 {
-            return .weak
-        } else {
-            return .lost
-        }
-    }
-}
+import KikurageFeature
 
 class WiFiSelectDeviceTableViewCell: UITableViewCell {
+    private let bleSignalImageView = UIImageView()
+    private let bleSignalLabel = UILabel()
     private let deviceNameLabel = UILabel()
     private let bleServiceNumberLabel = UILabel()
 
@@ -57,18 +27,52 @@ class WiFiSelectDeviceTableViewCell: UITableViewCell {
     private func setupComponent() {
         accessoryType = .disclosureIndicator
 
-        deviceNameLabel.font = .systemFont(ofSize: 17)
-        deviceNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        bleSignalImageView.image = KikurageBluetoothSignal().image
+        bleSignalImageView.translatesAutoresizingMaskIntoConstraints = false
 
-        addSubview(deviceNameLabel)
+        bleSignalLabel.text = "-"
+        bleSignalLabel.font = .systemFont(ofSize: 12)
+
+        let leftStackView = UIStackView()
+        leftStackView.axis = .vertical
+        leftStackView.alignment = .center
+        leftStackView.addArrangedSubview(bleSignalImageView)
+        leftStackView.addArrangedSubview(bleSignalLabel)
+        leftStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        deviceNameLabel.font = .systemFont(ofSize: 24)
+        deviceNameLabel.text = "Unnamed"
+
+        bleServiceNumberLabel.font = .systemFont(ofSize: 17)
+        bleServiceNumberLabel.textColor = .gray
+        bleServiceNumberLabel.text = "No services"
+
+        let rightStackView = UIStackView()
+        rightStackView.axis = .vertical
+        rightStackView.alignment = .leading
+        rightStackView.addArrangedSubview(deviceNameLabel)
+        rightStackView.addArrangedSubview(bleServiceNumberLabel)
+        rightStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(leftStackView)
+        addSubview(rightStackView)
 
         let margin: CGFloat = 8
+        let imageSize: CGFloat = 24
 
         NSLayoutConstraint.activate([
-            deviceNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: margin),
-            deviceNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: margin),
-            deviceNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -margin),
-            deviceNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -margin)
+            bleSignalImageView.widthAnchor.constraint(equalToConstant: imageSize),
+            bleSignalImageView.heightAnchor.constraint(equalToConstant: imageSize),
+
+            leftStackView.topAnchor.constraint(equalTo: topAnchor, constant: margin),
+            leftStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: margin * 2),
+            leftStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -margin),
+            leftStackView.widthAnchor.constraint(equalToConstant: imageSize * 1.5),
+
+            rightStackView.topAnchor.constraint(equalTo: topAnchor, constant: margin),
+            rightStackView.leadingAnchor.constraint(equalTo: leftStackView.trailingAnchor, constant: margin * 2),
+            rightStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -margin),
+            rightStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -margin)
         ])
     }
 }
