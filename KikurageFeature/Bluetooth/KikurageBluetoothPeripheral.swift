bdb2e60b82a5540a18903e27f569b1b776f1333e
shusuke0812 <shu.ota0812@gmail.com>
2023-03-06 22:14:34 +0900

    add: listCount, getElement members in KikurageBluetoothPeripheralList

--- KikurageFeature/Bluetooth/KikurageBluetoothPeripheral.swift
+++ KikurageFeature/Bluetooth/KikurageBluetoothPeripheral.swift
@@ -14,14 +14,14 @@ public struct KikurageBluetoothPeripheral {
     public let rssi: NSNumber
     public let peripheral: CBPeripheral
     public let uuid: UUID
-    
+
     init(advertisementData: [String: Any], rssi: NSNumber, peripheral: CBPeripheral) {
         self.advertisementData = advertisementData
         self.rssi = rssi
         self.peripheral = peripheral
         self.uuid = UUID(uuid: peripheral.identifier.uuid)
     }
-    
+
     public var deviceName: String {
         if let localName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
             return localName
@@ -29,15 +29,15 @@ public struct KikurageBluetoothPeripheral {
             return "Unnamed"
         }
     }
-    
+
     public var rssiInt: Int {
         rssi.intValue
     }
-    
+
     public var rssiString: String {
         "\(rssiInt)"
     }
-    
+
     public var serviceCountString: String {
         if let services = peripheral.services {
             return "\(services.count) services"
@@ -45,7 +45,7 @@ public struct KikurageBluetoothPeripheral {
             return "No services"
         }
     }
-    
+
     public func validateConnection() -> Bool {
         deviceName == KikurageBluetoothUUID.LocalName.debugM5Stack
     }
@@ -53,12 +53,20 @@ public struct KikurageBluetoothPeripheral {
 
 public struct KikurageBluetoothPeripheralList {
     private var list: [KikurageBluetoothPeripheral]
-    
+
     public init(list: [KikurageBluetoothPeripheral]) {
         self.list = list
     }
-    
+
     public mutating func add(peripheral: KikurageBluetoothPeripheral) {
         list.append(peripheral)
+    }
+
+    public var listCount: Int {
+        list.count
+    }
+
+    public func getElement(indexPath: IndexPath) -> KikurageBluetoothPeripheral {
+        list[indexPath.row]
     }
 }
