f155308689419ae16ffedee03f588090ad13505d
shusuke0812 <shu.ota0812@gmail.com>
2023-03-11 00:58:00 +0900

    fix: SwiftLint and SwiftFormat scripts

--- Scripts/SwiftFormat.sh
+++ Scripts/SwiftFormat.sh
@@ -5,10 +5,6 @@
 echo "start: ***** SwiftFormat *****"
 
 if which swiftlint > /dev/null; then
-  #git diff --name-only | grep .swift | while read filename; do
-    #swiftformat --path "$filename"
-  #done
-  #echo "success"
   swiftformat .
 else
   echo "warning: SwiftFormat not installed, download from https://github.com/nicklockwood/SwiftFormat"
