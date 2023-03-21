e4c49c58e2a7907152fdc451b054bb429428934a
shusuke0812 <shu.ota0812@gmail.com>
2023-03-21 20:19:22 +0900

    add: Realm delete method

--- Kikurage/Repository/Client/RealmClient.swift
+++ Kikurage/Repository/Client/RealmClient.swift
@@ -18,6 +18,8 @@ import RealmSwift
 protocol RealmClientProtocol {
     func createRequest<T: RealmSwift.Object>(_ object: T, completion: @escaping (Result<Void, Error>) -> Void)
     func readRequest<T: RealmSwift.Object>(id: String, completion: @escaping (Result<T, Error>) -> Void)
+    func deleteRequest<T: RealmSwift.Object>(_ object: T, completion: @escaping (Result<Void, Error>) -> Void)
+    func deleteAllRequest(completion: @escaping (Result<Void, Error>) -> Void)
 }
 
 struct RealmClient: RealmClientProtocol {
@@ -39,6 +41,28 @@ struct RealmClient: RealmClientProtocol {
             if let cache = realm.object(ofType: T.self, forPrimaryKey: T.primaryKey()) {
                 completion(.success(cache))
             }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func deleteRequest<T: RealmSwift.Object>(_ object: T, completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.delete(object)
+                completion(.success(()))
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func deleteAllRequest(completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            let realm = try Realm()
+            realm.deleteAll()
+            completion(.success(()))
         } catch {
             completion(.failure(error))
         }
