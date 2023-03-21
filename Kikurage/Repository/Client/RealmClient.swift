aaf9bbcc55427b12cda279b93f6a3e0e86d37b67
shusuke0812 <shu.ota0812@gmail.com>
2023-03-21 22:45:48 +0900

    add: comments

--- Kikurage/Repository/Client/RealmClient.swift
+++ Kikurage/Repository/Client/RealmClient.swift
@@ -13,6 +13,8 @@
  * - sort when read Object: https://www.mongodb.com/docs/realm/sdk/swift/crud/read/#sort-query-results
  * - chain queries when read Object: https://www.mongodb.com/docs/realm/sdk/swift/crud/read/#chain-queries
  * - limit of query: https://www.mongodb.com/docs/realm/sdk/swift/crud/read/#limiting-query-results
+ * - [ important ] across threads: https://www.mongodb.com/docs/realm/sdk/swift/crud/threading/#communication-across-threads
+ * - change listener: https://www.mongodb.com/docs/realm/sdk/swift/react-to-changes/#react-to-changes---swift-sdk
  */
 
 import Foundation
