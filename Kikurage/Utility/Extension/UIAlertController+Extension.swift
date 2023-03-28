2f311cbf6da5c3a039783e6a60734cb36d13373f
shusuke0812 <shu.ota0812@gmail.com>
2023-03-28 11:14:00 +0900

    fix: Logger in UIAlert+Extension

--- Kikurage/Utility/Extension/UIAlertController+Extension.swift
+++ Kikurage/Utility/Extension/UIAlertController+Extension.swift
@@ -11,19 +11,18 @@ import UIKit
 
 extension UIAlertController {
     static func showAlert(
-        style: UIAlertController.Style, viewController: UIViewController, title: String?, message: String?, okButtonTitle: String, cancelButtonTitle: String?, completionOk: (() -> Void)?,
-        file: String = #file, function: String = #function, line: Int = #line, logMessage: String = ""
+        style: UIAlertController.Style, viewController: UIViewController, title: String?, message: String?, okButtonTitle: String, cancelButtonTitle: String?, completionOk: (() -> Void)?, logMessage: String = ""
     ) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: style)
         // キャンセル｜OK というボタン配置にするためにCancelActionを先にAddする
         if let cancelButtonTitle = cancelButtonTitle {
             let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ in
-                KLogger.info(file: file, function: function, line: line, logMessage)
+                KLogManager.debug(logMessage)
             }
             alert.addAction(cancelAction)
         }
         let okAction = UIAlertAction(title: okButtonTitle, style: .default) { _ in
-            KLogger.info(file: file, function: function, line: line, logMessage)
+            KLogManager.debug(logMessage)
             if let completionOk = completionOk {
                 completionOk()
             }
