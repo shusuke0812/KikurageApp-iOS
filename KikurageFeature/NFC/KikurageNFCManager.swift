0568b9e7f77ac8f28102e8c355a0f67957418733
shusuke0812 <shu.ota0812@gmail.com>
2023-04-27 23:15:15 +0900

    add: NFC capability and manager

---
+++ KikurageFeature/NFC/KikurageNFCManager.swift
@@ -0,0 +1,13 @@
+//
+//  KikurageNFCManager.swift
+//  KikurageFeature
+//
+//  Created by Shusuke Ota on 2023/4/27.
+//  Copyright © 2023 shusuke. All rights reserved.
+//
+
+import Foundation
+
+public struct KikurageNFCManager {
+    
+}
