5962582aea89fe3d4068470f5c51acdf71431d3d
shusuke0812 <shu.ota0812@gmail.com>
2023-04-30 14:16:15 +0900

    add: NFC error type

---
+++ KikurageFeature/NFC/KikurageNFCError.swift
@@ -0,0 +1,23 @@
+//
+//  KikurageNFCError.swift
+//  KikurageFeature
+//
+//  Created by Shusuke Ota on 2023/4/29.
+//  Copyright © 2023 shusuke. All rights reserved.
+//
+
+import Foundation
+
+enum KikurageNFCError: Error {
+    case notAvailable
+    case messageGetFail
+    
+    var description: String {
+        switch self {
+        case .notAvailable:
+            return ResorceManager.getLocalizedString("nfc_available_fail_description")
+        case .messageGetFail:
+            return ResorceManager.getLocalizedString("nfc_message_get_fail_description")
+        }
+    }
+}
