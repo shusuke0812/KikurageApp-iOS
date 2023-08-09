055eec8ee710a678eb4dcdc686bba03742451058
shusuke0812 <shu.ota0812@gmail.com>
2023-08-10 08:34:34 +0900

    add: screen event type and sending method of Firebase anayltics

--- Kikurage/Utility/Manager/FirebaseAnalyticsManager.swift
+++ Kikurage/Utility/Manager/FirebaseAnalyticsManager.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2023 shusuke. All rights reserved.
 //
 
-import Foundation
 import FirebaseAnalytics
+import Foundation
 
-enum FirebaseAnalyticsEvent {
+enum FirebaseAnalyticsTapEvent {
     case debug
-    
+
     var name: String {
         switch self {
         case .debug:
@@ -20,12 +20,111 @@ enum FirebaseAnalyticsEvent {
     }
 }
 
+enum FirebaseAnalyticsScreenViewEvent {
+    case top
+    case login
+    case signUp
+    case deviceRegister
+    case home
+    case cultivation
+    case cultivationDetail
+    case postCultivation
+    case recipe
+    case postRecipe
+    case calendar
+    case graph
+    case accountSetting
+    case acknowledgement
+    case dictionary
+    case wifi
+    
+    var screenName: String {
+        switch self {
+        case .top:
+            return "Top"
+        case .login:
+            return "Login"
+        case .signUp:
+            return "SignUp"
+        case .deviceRegister:
+            return "DeviceRegister"
+        case .home:
+            return "Home"
+        case .cultivation:
+            return "Cultivation"
+        case .cultivationDetail:
+            return "CultivationDetail"
+        case .postCultivation:
+            return "PostCultivation"
+        case .recipe:
+            return "Recipe"
+        case .postRecipe:
+            return "PostRecipe"
+        case .calendar:
+            return "Calendar"
+        case .graph:
+            return "Graph"
+        case .accountSetting:
+            return "AccountSetting"
+        case .acknowledgement:
+            return "Acknowledgment"
+        case .dictionary:
+            return "Dictionary"
+        case .wifi:
+            return "WiFi"
+        }
+    }
+    
+    var screenClass: String {
+        switch self {
+        case .top:
+            return "app/top"
+        case .login:
+            return "app/login"
+        case .signUp:
+            return "app/sign_up"
+        case .deviceRegister:
+            return "app/device_register"
+        case .home:
+            return "app/home"
+        case .cultivation:
+            return "app/home/cultivation"
+        case .cultivationDetail:
+            return "app/home/cultivation/cultivation_detail"
+        case .postCultivation:
+            return "app/home/cultivation/post_cultivation"
+        case .recipe:
+            return "app/home/recipe"
+        case .postRecipe:
+            return "app/home/recipe/post_recipe"
+        case .calendar:
+            return "app/home/side_menu/calendar"
+        case .graph:
+            return "app/home/side_menu/graph"
+        case .accountSetting:
+            return "app/home/side_menu/account_setting"
+        case .acknowledgement:
+            return "app/home/side_menu/acknowledgment"
+        case .dictionary:
+            return "app/home/side_menu/dictionary"
+        case .wifi:
+            return "app/home/side_menu/wifi"
+        }
+    }
+}
+
 struct FirebaseAnalyticsManager {
-    static func sendEvent(_ event: FirebaseAnalyticsEvent) {
+    static func sendTapEvent(_ event: FirebaseAnalyticsTapEvent) {
         Analytics.logEvent(event.name, parameters: [
             AnalyticsParameterItemID: "id-debug-1234",
             AnalyticsParameterItemName: event.name,
             AnalyticsParameterContentType: "content"
+        ])
+    }
+    static func sendScreenViewEvent(_ event: FirebaseAnalyticsScreenViewEvent) {
+        Analytics.logEvent(AnalyticsEventScreenView, parameters: [
+            AnalyticsParameterScreenName: event.screenName,
+            AnalyticsParameterScreenClass: event.screenClass
         ])
     }
 }
