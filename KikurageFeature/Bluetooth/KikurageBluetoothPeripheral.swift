fc0068be774cd9b7ef078b34fd0427e4306b9bb9
shusuke0812 <shu.ota0812@gmail.com>
2023-11-27 00:09:48 +0900

    add: getElement(index: Int) in KikurageBluetooth

--- KikurageFeature/Bluetooth/KikurageBluetoothPeripheral.swift
+++ KikurageFeature/Bluetooth/KikurageBluetoothPeripheral.swift
@@ -69,4 +69,8 @@ public struct KikurageBluetoothPeripheralList {
     public func getElement(indexPath: IndexPath) -> KikurageBluetoothPeripheral {
         list[indexPath.row]
     }
+    
+    public func getElement(index: Int) -> KikurageBluetoothPeripheral {
+        list[index]
+    }
 }
