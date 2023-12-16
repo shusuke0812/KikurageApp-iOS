c9322ead96f849bd2a49f749f816f46890f1a553
shusuke0812 <shu.ota0812@gmail.com>
2023-12-16 21:30:19 +0900

    add: sign up code

--- Kikurage/Utility/Helper/LoginHelper.swift
+++ Kikurage/Utility/Helper/LoginHelper.swift
@@ -79,11 +79,15 @@ class LoginHelper {
     }
 
     func logout(onError: (() -> Void)? = nil) {
-        // FIXME: Auth.auth().signOutを追加
-        let rootVC = UIApplication.shared.windows.first?.rootViewController
-        if rootVC is AppRootController, let rootVC = rootVC as? AppRootController {
-            rootVC.logout(rootVC: rootVC)
-        } else {
+        do {
+            try Auth.auth().signOut()
+            let rootVC = UIApplication.shared.windows.first?.rootViewController
+            if rootVC is AppRootController, let rootVC = rootVC as? AppRootController {
+                rootVC.logout(rootVC: rootVC)
+            } else {
+                onError?()
+            }
+        } catch {
             onError?()
         }
     }
