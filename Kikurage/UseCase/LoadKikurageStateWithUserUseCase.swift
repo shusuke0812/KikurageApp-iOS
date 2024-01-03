c059202c320ac7cb6fd2ae6d76b732222bbde851
shusuke0812 <shu.ota0812@gmail.com>
2024-01-03 20:34:50 +0900

    add: LoadKikurageStateWithUserUseCase class

---
+++ Kikurage/UseCase/LoadKikurageStateWithUserUseCase.swift
@@ -0,0 +1,43 @@
+//
+//  LoadKikurageStateWithUserUseCase.swift
+//  Kikurage
+//
+//  Created by Shusuke Ota on 2024/1/3.
+//  Copyright © 2024 shusuke. All rights reserved.
+//
+
+import Foundation
+
+protocol LoadKikurageStateWithUserUseCaseProtocol {
+    func invoke(uid: String, completion: @escaping (Result<KikurageState, ClientError>) -> Void)
+}
+
+class LoadKikurageStateWithUserUseCase: LoadKikurageStateWithUserUseCaseProtocol {
+    private let kikurageStateRepository: KikurageStateRepositoryProtocol
+    private let kikurageUserRepository: KikurageUserRepositoryProtocol
+    
+    init(kikurageStateRepository: KikurageStateRepositoryProtocol, kikurageUserRepository: KikurageUserRepositoryProtocol) {
+        self.kikurageStateRepository = kikurageStateRepository
+        self.kikurageUserRepository = kikurageUserRepository
+    }
+    
+    func invoke(uid: String, completion: @escaping (Result<KikurageState, ClientError>) -> Void) {
+        let userRequest = KikurageUserRequest(uid: uid)
+        kikurageUserRepository.getKikurageUser(request: userRequest) { [weak self] response in
+            switch response {
+            case .success(let kikurageUser):
+                let stateRequest = KikurageStateRequest(productID: kikurageUser.productKey)
+                self?.kikurageStateRepository.getKikurageState(request: stateRequest) { response in
+                    switch response {
+                    case .success(let kikurageState):
+                        completion(.success(kikurageState))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
