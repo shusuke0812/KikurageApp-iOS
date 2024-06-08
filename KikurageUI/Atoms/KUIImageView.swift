1137899190c4fcc15440fa90ef5851d7cf2df56d
shusuke0812 <shu.ota0812@gmail.com>
2024-06-08 14:13:15 +0900

    add: KUIImageView and CGFloat+Extension files

---
+++ KikurageUI/Atoms/KUIImageView.swift
@@ -0,0 +1,21 @@
+//
+//  KUIImageView.swift
+//  KikurageUI
+//
+//  Created by Shusuke Ota on 2024/6/8.
+//  Copyright © 2024 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class KUIImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ KikurageUI/CGFloat+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  CGFloat+Extension.swift
+//  KikurageUI
+//
+//  Created by Shusuke Ota on 2024/6/8.
+//  Copyright © 2024 shusuke. All rights reserved.
+//
+
+import Foundation
