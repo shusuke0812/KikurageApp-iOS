97476043baec0857a8f483a2fa5f685f411f5489
shusuke0812 <shu.ota0812@gmail.com>
2024-06-08 14:14:35 +0900

    fix: directory

--- KikurageUI/CGFloat+Extension.swift
+++ KikurageUI/CGFloat+Extension.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+extension CGFloat {
+    // Corner radius
+    static let viewCornerRadius: CGFloat = 18
+    static let buttonCornerRadius: CGFloat = 5
+    static let cellCornerRadius: CGFloat = 10
+
+    // Collection view
+    static let cellSpacing: CGFloat = 4
+
+    // Aspect ratio
+    static let imageViewRatio: CGFloat = 9 / 16
+}
