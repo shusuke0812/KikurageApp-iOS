f8286b19f11244fc0f52c8227a2f35fe339546da
shusuke0812 <shu.ota0812@gmail.com>
2024-10-28 09:44:10 +0900

    remove: KUIDeviceStatusImageView props

--- KikurageUI/Organisms/KUIDeviceStatusImageView.swift
+++ KikurageUI/Organisms/KUIDeviceStatusImageView.swift
@@ -6,65 +6,41 @@
 //  Copyright © 2024 shusuke. All rights reserved.
 //
 
-import KikurageDomain
 import UIKit
-
-public struct KUIDeviceStatusImageViewProps {
-    let state: KikurageState
-    let dateString: String
-
-    public init(state: KikurageState, dateString: String) {
-        self.state = state
-        self.dateString = dateString
-    }
-    
-    func setStateImages() -> [UIImage] {
-        guard let type = state.type else {
-            return []
-        }
-        var kikurageStateImages: [UIImage] = []
-        let beforeImage = UIImage(named: "\(type.rawValue)_01")! // swiftlint:disable:this force_unwrapping
-        let afterImage = UIImage(named: "\(type.rawValue)_02")! // swiftlint:disable:this force_unwrapping
-
-        kikurageStateImages.append(beforeImage)
-        kikurageStateImages.append(afterImage)
-
-        return kikurageStateImages
-    }
-}
 
 public class KUIDeviceStatusImageView: UIView {
     private var statusImageView: UIImageView!
 
-    public init(props: KUIDeviceStatusImageViewProps) {
-        super.init(frame: .zero)
-        setupComponent(props: props)
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setupComponent()
     }
 
-    public required init?(coder: NSCoder) {
-        nil
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupComponent()
     }
 
-    public func runAnimation(props: KUIDeviceStatusImageViewProps) {
-        statusImageView.animationImages = props.setStateImages()
+    public func runAnimation(images: [UIImage]) {
+        statusImageView.animationImages = images
         statusImageView.animationDuration = 1
         statusImageView.animationRepeatCount = 0
         statusImageView.startAnimating()
     }
-    
+
     public func startAnimating() {
         statusImageView.startAnimating()
     }
-    
+
     public func stopAnimating() {
         statusImageView.stopAnimating()
     }
 
-    private func setupComponent(props: KUIDeviceStatusImageViewProps) {
+    private func setupComponent() {
         let parentView = KUIRoundedView()
         parentView.translatesAutoresizingMaskIntoConstraints = false
 
-        statusImageView = UIImageView() // TODO: imageの設定
+        statusImageView = UIImageView()
         statusImageView.contentMode = .scaleAspectFill
         statusImageView.translatesAutoresizingMaskIntoConstraints = false
 
