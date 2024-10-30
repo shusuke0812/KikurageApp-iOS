61672e87ec0da8710df671bea5eebeacaf8a1cb6
shusuke0812 <shu.ota0812@gmail.com>
2024-10-30 09:58:49 +0900

    change: base view initialization from using IBOutlet to programmatically

--- Kikurage/Screen/Communication/BaseView/CommunicationBaseView.swift
+++ Kikurage/Screen/Communication/BaseView/CommunicationBaseView.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2021 shusuke. All rights reserved.
 //
 
+import KikurageUI
 import UIKit
 
 protocol CommunicationBaseViewDelegate: AnyObject {
@@ -13,32 +14,77 @@ protocol CommunicationBaseViewDelegate: AnyObject {
 }
 
 class CommunicationBaseView: UIView {
-    @IBOutlet private weak var informationLabel: UILabel!
-    @IBOutlet private weak var informationParentView: UIView!
+    private var imageView: UIImageView!
+    private var informationView: KUIRoundedView!
+    private var informationLabel: UILabel!
+    private var facebookButton: UIButton!
 
     weak var delegate: CommunicationBaseViewDelegate?
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        initUI()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupComponent()
+        setupButtonAction()
     }
 
-    // MARK: - Action
-
-    @IBAction private func openFacebook(_ sender: Any) {
-        delegate?.communicationBaseViewDidTapFacebookButton(self)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
 // MARK: - Initialized
 
 extension CommunicationBaseView {
-    private func initUI() {
+    private func setupComponent() {
         backgroundColor = .systemGroupedBackground
 
-        informationLabel.text = R.string.localizable.screen_communication_information()
+        imageView = UIImageView(image: R.image.communication())
+        imageView.translatesAutoresizingMaskIntoConstraints = false
 
-        informationParentView.clipsToBounds = true
-        informationParentView.layer.cornerRadius = .viewCornerRadius
+        informationView = KUIRoundedView(props: KUIRoundedViewProps())
+        informationView.translatesAutoresizingMaskIntoConstraints = false
+
+        informationLabel = UILabel()
+        informationLabel.text = R.string.localizable.screen_communication_information()
+        informationLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        facebookButton = UIButton()
+        facebookButton.translatesAutoresizingMaskIntoConstraints = false
+
+        informationView.addSubview(informationView)
+        addSubview(imageView)
+        addSubview(informationView)
+        addSubview(facebookButton)
+
+        NSLayoutConstraint.activate([
+            informationLabel.topAnchor.constraint(equalTo: informationView.topAnchor, constant: 10),
+            informationView.leadingAnchor.constraint(equalTo: informationView.leadingAnchor, constant: 15),
+            informationView.trailingAnchor.constraint(equalTo: informationView.trailingAnchor, constant: -15),
+            informationView.bottomAnchor.constraint(equalTo: informationView.bottomAnchor, constant: -10)
+        ])
+
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1),
+            imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 50),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50),
+
+            informationView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            informationView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            informationView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+
+            facebookButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            facebookButton.topAnchor.constraint(equalTo: informationView.bottomAnchor, constant: 30),
+            facebookButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)
+        ])
+    }
+
+    private func setupButtonAction() {
+        facebookButton.addAction(.init { [weak self] _ in
+            guard let self else {
+                return
+            }
+            self.delegate?.communicationBaseViewDidTapFacebookButton(self)
+        }, for: .touchUpInside)
     }
 }
