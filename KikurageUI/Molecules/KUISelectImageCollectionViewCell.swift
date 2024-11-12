ef8dedf18f3d5973fab00102664332ec466d0a05
shusuke0812 <shu.ota0812@gmail.com>
2024-11-12 09:48:13 +0900

    add: camera image in KikurageUI asset

---
+++ KikurageUI/Molecules/KUISelectImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  KUISelectImageCollectionViewCell.swift
+//  KikurageUI
+//
+//  Created by Shusuke Ota on 2024/11/7.
+//  Copyright © 2024 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class KUISelectImageCollectionViewCell: UICollectionViewCell {
+    
+}
