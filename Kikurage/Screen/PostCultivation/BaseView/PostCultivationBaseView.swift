d304e37f73f751fa923658b101efa66864920b42
shusuke0812 <shu.ota0812@gmail.com>
2024-12-08 16:21:06 +0900

    fix: paddings and remove unnecessary methods

--- Kikurage/Screen/PostCultivation/BaseView/PostCultivationBaseView.swift
+++ Kikurage/Screen/PostCultivation/BaseView/PostCultivationBaseView.swift
@@ -24,25 +24,25 @@ class PostCultivationBaseView: UIView {
     weak var delegate: PostCultivationBaseViewDelegate?
 
     // MARK: - Lifecycle
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupComponent()
         setupAction()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func configCollectionView(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource) {
         cameraCollectionView.delegate = delegate
         cameraCollectionView.dataSource = dataSource
     }
-    
+
     private func setupComponent() {
         backgroundColor = .systemGroupedBackground
-        
+
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.itemSize = CGSize(width: 80, height: 80)
         flowLayout.minimumLineSpacing = 10
@@ -51,52 +51,53 @@ class PostCultivationBaseView: UIView {
         cameraCollectionView.backgroundColor = .systemGroupedBackground
         cameraCollectionView.register(KUISelectImageCollectionViewCell.self, forCellWithReuseIdentifier: KUISelectImageCollectionViewCell.identifier)
         cameraCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         cultivationMemoTextView = KUIMaterialTextView(props: KUIMaterialTextViewProps(
             maxTextCount: 200,
             placeHolder: R.string.localizable.screen_post_cultivation_textview_placeholder(),
             backgroundColor: .systemGroupedBackground
         ))
         cultivationMemoTextView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         dateTextField = KUIDropdownTextField(props: KUIDropDownTextFieldProps(
             variant: .date,
             textFieldProps: KUITextFieldProps(placeHolder: R.string.localizable.screen_post_cultivation_date_textfield_placeholder())
         ))
         dateTextField.translatesAutoresizingMaskIntoConstraints = false
-        
+
         postButton = KUIButton(props: KUIButtonProps(
             variant: .primary,
             title: R.string.localizable.screen_post_cultivation_post_button_title()
         ))
         postButton.translatesAutoresizingMaskIntoConstraints = false
-        
+
         addSubview(cameraCollectionView)
         addSubview(cultivationMemoTextView)
         addSubview(dateTextField)
         addSubview(postButton)
-        
+
         NSLayoutConstraint.activate([
             cameraCollectionView.heightAnchor.constraint(equalToConstant: 180),
             cameraCollectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 30),
             cameraCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
             cameraCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-            
+
+            cultivationMemoTextView.heightAnchor.constraint(equalToConstant: 70),
             cultivationMemoTextView.topAnchor.constraint(equalTo: cameraCollectionView.bottomAnchor, constant: 15),
             cultivationMemoTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
             cultivationMemoTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-            
+
             dateTextField.topAnchor.constraint(equalTo: cultivationMemoTextView.bottomAnchor, constant: 20),
             dateTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
             dateTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-            
+
             postButton.heightAnchor.constraint(equalToConstant: 45),
             postButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             postButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             postButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 10)
         ])
     }
-    
+
     private func setupAction() {
         postButton.onTap = { [weak self] in
             guard let self else {
@@ -104,14 +105,14 @@ class PostCultivationBaseView: UIView {
             }
             self.delegate?.postCultivationBaseViewDidTappedPostButton(self)
         }
-        
+
         cultivationMemoTextView.onDidEndEditing = { [weak self] text in
             guard let self else {
                 return
             }
             self.delegate?.postCultivationBaseViewDidEndEditingCultivationMemo(self, text: text)
         }
-        
+
         dateTextField.onDidEndEditing = { [weak self] (date: Date) in
             guard let self else {
                 return
@@ -119,25 +120,4 @@ class PostCultivationBaseView: UIView {
             self.delegate?.postCultivationBaseViewDidEndEditingCultivationDate(self, date: date)
         }
     }
-}
-
-// MARK: - Config
-
-extension PostCultivationBaseView {
-//    func setCurrentTextViewNumber(text: String) {
-//        currentTextViewNumberLabel.text = "\(text.count)"
-//    }
-//
-//    func configCollectionView(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource) {
-//        cameraCollectionView.delegate = delegate
-//        cameraCollectionView.dataSource = dataSource
-//    }
-//
-//    func configTextView(delegate: UITextViewDelegate) {
-//        textView.delegate = delegate
-//    }
-//
-//    func configTextField(delegate: UITextFieldDelegate) {
-//        dateTextField.delegate = delegate
-//    }
 }
