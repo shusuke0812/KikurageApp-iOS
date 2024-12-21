be6350a134efd7d8b42074f32797714131590515
shusuke0812 <shu.ota0812@gmail.com>
2024-12-21 13:08:18 +0900

    fix: constraint and properties

--- KikurageUI/Organisms/KUITweetTableViewCell.swift
+++ KikurageUI/Organisms/KUITweetTableViewCell.swift
@@ -13,13 +13,13 @@ public struct KUITweetTableViewCellProps {
     let userName: String
     let createdAtString: String
     let tweet: String
-    let iconImageUrl: URL
-    
-    public init(userName: String, createdAtString: String, tweet: String, iconImageUrl: URL) {
+    let iconImageURL: URL?
+
+    public init(userName: String, createdAtString: String, tweet: String, iconImageURL: URL?) {
         self.userName = userName
         self.createdAtString = createdAtString
         self.tweet = tweet
-        self.iconImageUrl = iconImageUrl
+        self.iconImageURL = iconImageURL
     }
 }
 
@@ -31,37 +31,39 @@ public class KUITweetTableViewCell: UITableViewCell {
     private var createdAtLabel: UILabel!
     private var tweetLabel: UILabel!
 
+    private let iconImageWidth: CGFloat = 50
+
     override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupComponent()
     }
-    
+
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public func updateItem(props: KUITweetTableViewCellProps) {
         userNameLabel.text = props.userName
         createdAtLabel.text = props.createdAtString
         tweetLabel.text = props.tweet
-        userIconImageView.sd_setImage(with: props.iconImageUrl)
+        userIconImageView.sd_setImage(with: props.iconImageURL)
     }
-    
+
     private func setupComponent() {
         userIconImageView = UIImageView()
         userIconImageView.clipsToBounds = true
-        userIconImageView.layer.cornerRadius = userIconImageView.frame.width / 2
+        userIconImageView.layer.cornerRadius = iconImageWidth / 2
         userIconImageView.layer.borderColor = UIColor.lightGray.cgColor
         userIconImageView.layer.borderWidth = 0.3
         userIconImageView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         userNameLabel = UILabel()
         userNameLabel.font = .boldSystemFont(ofSize: 17.0)
         userNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
+
         createdAtLabel = UILabel()
         createdAtLabel.translatesAutoresizingMaskIntoConstraints = false
-        
+
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.alignment = .fill
@@ -69,7 +71,7 @@ public class KUITweetTableViewCell: UITableViewCell {
         stackView.spacing = 8
         stackView.addArrangedSubview(userNameLabel)
         stackView.addArrangedSubview(createdAtLabel)
-        
+
         let headerStackView = UIStackView()
         headerStackView.axis = .horizontal
         headerStackView.alignment = .fill
@@ -78,24 +80,23 @@ public class KUITweetTableViewCell: UITableViewCell {
         headerStackView.addArrangedSubview(userIconImageView)
         headerStackView.addArrangedSubview(stackView)
         headerStackView.translatesAutoresizingMaskIntoConstraints = false
-        
+
+        tweetLabel = UILabel()
         tweetLabel.font = .systemFont(ofSize: 17.0, weight: .light)
         tweetLabel.contentMode = .left
         tweetLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        contentView.addSubview(userIconImageView)
-        contentView.addSubview(userNameLabel)
-        contentView.addSubview(createdAtLabel)
+
+        contentView.addSubview(headerStackView)
         contentView.addSubview(tweetLabel)
-        
+
         NSLayoutConstraint.activate([
-            userIconImageView.heightAnchor.constraint(equalToConstant: 50),
-            userIconImageView.widthAnchor.constraint(equalToConstant: 50),
-            
+            userIconImageView.heightAnchor.constraint(equalToConstant: iconImageWidth),
+            userIconImageView.widthAnchor.constraint(equalToConstant: iconImageWidth),
+
             headerStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
             headerStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
             headerStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
-            
+
             tweetLabel.topAnchor.constraint(equalTo: headerStackView.bottomAnchor, constant: 8),
             tweetLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
             tweetLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
