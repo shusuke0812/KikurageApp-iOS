ff488af223af6e6587e7422fff03fc1e83279c84
shusuke0812 <shu.ota0812@gmail.com>
2024-12-28 13:38:57 +0900

    remove: unused method

--- Kikurage/Screen/SignUp/ViewController/SignUpViewController.swift
+++ Kikurage/Screen/SignUp/ViewController/SignUpViewController.swift
@@ -29,10 +29,6 @@ class SignUpViewController: UIViewController, UIViewControllerNavigatable, SignU
         adjustNavigationBarBackgroundColor()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         FirebaseAnalyticsHelper.sendScreenViewEvent(.signUp)
