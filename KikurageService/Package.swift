8b224489135197c6f634970d0118ee75637988f9
shusuke0812 <shu.ota0812@gmail.com>
2024-12-30 16:07:05 +0900

    change: KikurageFeature from embeded framework to SPM, and rename to `KikurageService`

---
+++ KikurageService/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KikurageService",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "KikurageService",
+            targets: ["KikurageService"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "KikurageService",
+            dependencies: [
+            ]
+        ),
+        .testTarget(
+            name: "KikurageServiceTests",
+            dependencies: ["KikurageService"]
+        ),
+    ]
+)
