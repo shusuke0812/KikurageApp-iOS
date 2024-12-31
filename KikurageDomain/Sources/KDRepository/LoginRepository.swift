2d972ec84ebb625ea8eaf389f1c2e9544b586039
shusuke0812 <shu.ota0812@gmail.com>
2024-12-31 19:44:27 +0900

    fix: apply FirebaseAuthClient to LoginRepository

--- KikurageDomain/Sources/KDRepository/LoginRepository.swift
+++ KikurageDomain/Sources/KDRepository/LoginRepository.swift
@@ -7,31 +7,36 @@
 //
 
 import KDFirebase
-import KDRestApi
 
 protocol LoginRepositoryProtocol {
-    func login(loginInfo: (email: String, password: String), completion: @escaping (Result<LoginUser, ClientError>) -> Void)
+    func login(loginInfo: (email: String, password: String), completion: @escaping (Result<LoginUser, FirebaseClientError>) -> Void)
 }
 
-public class LoginRepository: LoginRepositoryProtocol {}
+public class LoginRepository: LoginRepositoryProtocol {
+    private let firebaseAuthClient: FirebaseAuthClientProtocol
+    
+    public init(firebaseAuthClient: FirebaseAuthClientProtocol = FirebaseAuthClient()) {
+        self.firebaseAuthClient = firebaseAuthClient
+    }
+}
 
 // MARK: - Firebase Authentication
 
 extension LoginRepository {
-    func login(loginInfo: (email: String, password: String), completion: @escaping (Result<LoginUser, ClientError>) -> Void) {
-        Auth.auth().signIn(withEmail: loginInfo.email, password: loginInfo.password) { authResult, error in
-            if let error = error {
-                dump(error)
-                completion(.failure(ClientError.apiError(.readError)))
-                return
+    func login(loginInfo: (email: String, password: String), completion: @escaping (Result<LoginUser, FirebaseClientError>) -> Void) {
+        firebaseAuthClient.login(loginInfo: loginInfo) { result in
+            switch result {
+            case .success(let authDataResult):
+                guard let user = authDataResult?.user else {
+                    completion(.failure(.apiError(.readError)))
+                    return
+                }
+                let loginUser = LoginUser(uid: user.uid, isEmailVerified: user.isEmailVerified)
+                //TODO: ローカルストアに保存する（LoginHelper.shared.setUserInUserDefaults(user: loginUser)）
+                completion(.success(loginUser))
+            case .failure(let error):
+                completion(.failure(error))
             }
-            guard let user = authResult?.user else {
-                completion(.failure(ClientError.apiError(.readError)))
-                return
-            }
-            let loginUser = LoginUser(uid: user.uid, isEmailVerified: user.isEmailVerified)
-            LoginHelper.shared.setUserInUserDefaults(user: loginUser)
-            completion(.success(loginUser))
         }
     }
 }
