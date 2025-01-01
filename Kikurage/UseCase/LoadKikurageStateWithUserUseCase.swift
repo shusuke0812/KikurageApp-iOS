d80de930fcb8e7db9d258af9aa9df06fc4ba5906
shusuke0812 <shu.ota0812@gmail.com>
2025-01-01 14:19:14 +0900

    fix: build error in LoadKikurageStateWithUserUseCase

--- Kikurage/UseCase/LoadKikurageStateWithUserUseCase.swift
+++ Kikurage/UseCase/LoadKikurageStateWithUserUseCase.swift
@@ -12,7 +12,7 @@ import KDRepository
 typealias KikurageStateUserTuple = (user: KikurageUser, state: KikurageState)
 
 protocol LoadKikurageStateWithUserUseCaseProtocol {
-    func invoke(uid: String, completion: @escaping (Result<KikurageStateUserTuple, ClientError>) -> Void)
+    func invoke(uid: String, completion: @escaping (Result<KikurageStateUserTuple, Error>) -> Void)
 }
 
 class LoadKikurageStateWithUserUseCase: LoadKikurageStateWithUserUseCaseProtocol {
@@ -24,7 +24,7 @@ class LoadKikurageStateWithUserUseCase: LoadKikurageStateWithUserUseCaseProtocol
         self.kikurageUserRepository = kikurageUserRepository
     }
 
-    func invoke(uid: String, completion: @escaping (Result<KikurageStateUserTuple, ClientError>) -> Void) {
+    func invoke(uid: String, completion: @escaping (Result<KikurageStateUserTuple, Error>) -> Void) {
         let userRequest = KikurageUserRequest(uid: uid)
         kikurageUserRepository.getKikurageUser(request: userRequest) { [weak self] response in
             switch response {
