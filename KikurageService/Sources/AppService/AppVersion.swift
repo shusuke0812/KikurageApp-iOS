a384cefa30f60c5b699841ff79b51251d591b2a6
shusuke0812 <shu.ota0812@gmail.com>
2025-01-02 16:11:53 +0900

    move: AppVersion class to KikurageService

---
+++ KikurageService/Sources/AppService/AppVersion.swift
@@ -0,0 +1,65 @@
+//
+//  File.swift
+//  KikurageService
+//
+//  Created by Shusuke Ota on 2025/1/2.
+//
+
+import Foundation
+
+public struct AppVersion {
+    public let major: Int
+    public let minor: Int
+    public let patch: Int
+    
+    private let separator: Character = "."
+    
+    public init?(versionString: String) {
+        guard let versions = versions(versionString) else {
+            return nil
+        }
+    }
+    
+    public var versionString: String {
+        "\(major)\(separator)\(minor)\(separator)\(patch)"
+    }
+    
+    private func versions(_ versionString: String) -> [Int]? {
+        let versions = versionString.split(separator: separator).compactMap { Int($0) }
+        return (versions.count == 3) ? versions : nil
+    }
+    
+    // MARK: Comparable
+
+    public static func < (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        if lhs.major < rhs.major {
+            return true
+        } else if lhs.major > rhs.major {
+            return false
+        }
+        
+        if lhs.minor < rhs.minor {
+            return true
+        } else if lhs.minor > rhs.minor {
+            return false
+        }
+        
+        if lhs.patch < rhs.patch {
+            return true
+        }
+        return false
+    }
+    
+    // MARK: Equatable
+    
+    public static func == (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        if lhs.major == rhs.major {
+            return true
+        } else if lhs.minor == rhs.minor {
+            return true
+        } else if lhs.patch == rhs.patch {
+            return true
+        }
+        return false
+    }
+}
