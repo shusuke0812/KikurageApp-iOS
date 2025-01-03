9ddcd9dbc27b65be8b53d20de62f21e4526f1f90
shusuke0812 <shu.ota0812@gmail.com>
2025-01-03 14:03:51 +0900

    add: UserDefaultsRequestProtocol

---
+++ KikurageDomain/Sources/Infrastructure/KDLocalStore/LocalStoreRequestProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  LocalStoreRequestProtocol.swift
+//  KikurageDomain
+//
+//  Created by Shusuke Ota on 2025/1/3.
+//
+
+import Foundation
+
+/**
+ * NOTE:
+ * When custon login user in UserDefaults,
+ * archive and unarchive processing of these data must conform to NSObject and NSSecureCoding protocols
+ */
+public protocol UserDefaultsRequestProtocol {
+    associatedtype Response: NSObject, NSSecureCoding
+    
+    var key: String { get }
+}
