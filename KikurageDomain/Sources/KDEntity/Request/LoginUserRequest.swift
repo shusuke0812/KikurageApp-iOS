3befb5e23d720e5c20a0806874e24bd5022a7bad
shusuke0812 <shu.ota0812@gmail.com>
2025-01-03 15:24:53 +0900

    add: LoginUserRequest

---
+++ KikurageDomain/Sources/KDEntity/Request/LoginUserRequest.swift
@@ -0,0 +1,22 @@
+//
+//  LoginUserRequest.swift
+//  KikurageDomain
+//
+//  Created by Shusuke Ota on 2025/1/3.
+//
+
+import KDLocalStore
+import Foundation
+
+public struct LoginUserRequest: UserDefaultsRequestProtocol {
+    public typealias Response = LoginUser
+    
+    public var key: String {
+        "firebase_user"
+    }
+    public var saveData: LoginUser
+    
+    public init(loginUser: LoginUser) {
+        self.saveData = loginUser
+    }
+}
