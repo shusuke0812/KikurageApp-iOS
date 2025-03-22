ffd03a342d912d8991a4e1b8c48c8ff454d323c0
shusuke0812 <shu.ota0812@gmail.com>
2025-03-22 22:25:55 +0900

    Fix AccessibilitIdentifiable

--- Kikurage/Utility/Manager/AccessibilitIdentifiable.swift
+++ Kikurage/Utility/Manager/AccessibilitIdentifiable.swift
@@ -31,32 +31,3 @@ fileprivate func className(from filepath: String) -> String {
     let fileName = filepath.components(separatedBy: "/").last
     return fileName?.components(separatedBy: ".").first ?? ""
 }
-
-//enum AccessibilityIdentifierManager {
-//    // MARK: Common
-//
-//    private static func className(from filepath: String) -> String {
-//        let fileName = filepath.components(separatedBy: "/").last
-//        return fileName?.components(separatedBy: ".").first ?? ""
-//    }
-//
-//    // MARK: Screen
-//
-//    // Top
-//    static func topLoginButton(file: String = #file) -> String {
-//        className(from: file) + "_" + "login_button"
-//    }
-//
-//    // Login
-//    static func loginEmailTextField(file: String = #file) -> String {
-//        className(from: file) + "_" + "email_textfield"
-//    }
-//
-//    static func loginPasswordTextField(file: String = #file) -> String {
-//        className(from: file) + "_" + "password_textfield"
-//    }
-//
-//    static func loginLoginButton(file: String = #file) -> String {
-//        className(from: file) + "_" + "login_button"
-//    }
-//}
